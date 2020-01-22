82df35e58b1ceb79d362f89f8897d37d99551725
Sergio C. Arteaga <tegioz@icloud.com>
2020-01-22 12:49:38 +0100

    Add get_stats func to drop_all_functions.sql (#32)
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- database/migrations/functions/drop_all_functions.sql
+++ database/migrations/functions/drop_all_functions.sql
@@ -1,6 +1,7 @@
-drop function if exists search_packages(query jsonb);
 drop function if exists get_package(p_package_id uuid);
 drop function if exists get_package_version(p_package_id uuid, version text);
+drop function if exists search_packages(query jsonb);
+drop function if exists get_stats();
 drop function if exists register_package(p_pkg jsonb);
 drop function if exists get_chart_repository_packages_digest(p_chart_repository_id uuid);
 drop function if exists get_chart_repository_by_name(name text);
