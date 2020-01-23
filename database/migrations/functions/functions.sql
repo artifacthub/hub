aa186f8d64ffbabee806677c078ed552fc06468f
Sergio Castaño Arteaga <tegioz@icloud.com>
2020-01-23 13:15:57 +0100

    Fix issue tracking package last version
    Closes #35
    
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- database/migrations/functions/functions.sql
+++ database/migrations/functions/functions.sql
@@ -54,96 +54,87 @@ create or replace function register_package(p_pkg jsonb)
 returns void as $$
 declare
     v_package_id uuid;
-    v_latest_version text;
     v_chart_repository_id text := (p_pkg->'chart_repository')->>'chart_repository_id';
     v_package_latest_version_needs_update boolean := false;
     v_maintainer jsonb;
     v_maintainer_id uuid;
 begin
-    -- Identify if the package latest version needs to be updated
-    case p_pkg->>'kind'
-        when '0' then -- Chart
-            select package_id, latest_version
-            into v_package_id, v_latest_version
-            from package
-            where chart_repository_id = v_chart_repository_id::uuid
-            and name = p_pkg->>'name';
+    -- Package
+    insert into package (
+        name,
+        display_name,
+        description,
+        home_url,
+        logo_url,
+        keywords,
+        latest_version,
+        package_kind_id,
+        chart_repository_id
+    ) values (
+        p_pkg->>'name',
+        nullif(p_pkg->>'display_name', ''),
+        nullif(p_pkg->>'description', ''),
+        nullif(p_pkg->>'home_url', ''),
+        nullif(p_pkg->>'logo_url', ''),
+        (select (array(select jsonb_array_elements_text(nullif(p_pkg->'keywords', 'null'::jsonb))))::text[]),
+        p_pkg->>'version',
+        (p_pkg->>'kind')::int,
+        nullif(v_chart_repository_id, '')::uuid
+    )
+    on conflict (chart_repository_id, name) do update
+    set
+        name = excluded.name,
+        display_name = excluded.display_name,
+        description = excluded.description,
+        home_url = excluded.home_url,
+        keywords = excluded.keywords,
+        latest_version = excluded.latest_version
+    where semver_gte(p_pkg->>'version', package.latest_version) = true
+    returning package_id into v_package_id;
 
-            if not found or semver_gte(p_pkg->>'version', v_latest_version) then
-                v_package_latest_version_needs_update := true;
-            end if;
-    end case;
-
-    if v_package_latest_version_needs_update then
-        -- Package
-        insert into package (
-            name,
-            display_name,
-            description,
-            home_url,
-            logo_url,
-            keywords,
-            latest_version,
-            package_kind_id,
-            chart_repository_id
-        ) values (
-            p_pkg->>'name',
-            nullif(p_pkg->>'display_name', ''),
-            nullif(p_pkg->>'description', ''),
-            nullif(p_pkg->>'home_url', ''),
-            nullif(p_pkg->>'logo_url', ''),
-            (select (array(select jsonb_array_elements_text(nullif(p_pkg->'keywords', 'null'::jsonb))))::text[]),
-            p_pkg->>'version',
-            (p_pkg->>'kind')::int,
-            nullif(v_chart_repository_id, '')::uuid
-        )
-        on conflict (chart_repository_id, name) do update
-        set
-            name = excluded.name,
-            display_name = excluded.display_name,
-            description = excluded.description,
-            home_url = excluded.home_url,
-            keywords = excluded.keywords,
-            latest_version = excluded.latest_version
-        returning package_id into v_package_id;
-
-        -- Maintainers
-        for v_maintainer in select * from jsonb_array_elements(nullif(p_pkg->'maintainers', 'null'::jsonb))
-        loop
-            -- Register maintainer if needed
-            insert into maintainer (name, email)
-            values (v_maintainer->>'name', v_maintainer->>'email')
-            on conflict (email) do nothing
-            returning maintainer_id into v_maintainer_id;
-
-            -- If maintainer was already registered, get maintainer id
-            if not found then
-                select maintainer_id into v_maintainer_id
-                from maintainer
-                where email = v_maintainer->>'email';
-            end if;
-
-            -- Bind package to maintainer
-            insert into package__maintainer (package_id, maintainer_id)
-            values (v_package_id, v_maintainer_id)
-            on conflict do nothing;
-        end loop;
-
-        -- Unbind deleted maintainers from package
-        delete from package__maintainer
-        where package_id = v_package_id
-        and maintainer_id not in (
-            select maintainer_id from maintainer where email in (
-                select value->>'email'
-                from jsonb_array_elements(nullif(p_pkg->'maintainers', 'null'::jsonb))
-            )
-        );
-
-        -- Clean up orphan maintainers not bound to any package
-        delete from maintainer where maintainer_id not in (
-            select maintainer_id from package__maintainer
-        );
+    if not found then
+        select package_id into v_package_id
+        from package
+        where chart_repository_id = v_chart_repository_id::uuid
+        and name = p_pkg->>'name';
     end if;
+
+    -- Maintainers
+    for v_maintainer in select * from jsonb_array_elements(nullif(p_pkg->'maintainers', 'null'::jsonb))
+    loop
+        -- Register maintainer if needed
+        insert into maintainer (name, email)
+        values (v_maintainer->>'name', v_maintainer->>'email')
+        on conflict (email) do nothing
+        returning maintainer_id into v_maintainer_id;
+
+        -- If maintainer was already registered, get maintainer id
+        if not found then
+            select maintainer_id into v_maintainer_id
+            from maintainer
+            where email = v_maintainer->>'email';
+        end if;
+
+        -- Bind package to maintainer
+        insert into package__maintainer (package_id, maintainer_id)
+        values (v_package_id, v_maintainer_id)
+        on conflict do nothing;
+    end loop;
+
+    -- Unbind deleted maintainers from package
+    delete from package__maintainer
+    where package_id = v_package_id
+    and maintainer_id not in (
+        select maintainer_id from maintainer where email in (
+            select value->>'email'
+            from jsonb_array_elements(nullif(p_pkg->'maintainers', 'null'::jsonb))
+        )
+    );
+
+    -- Clean up orphan maintainers not bound to any package
+    delete from maintainer where maintainer_id not in (
+        select maintainer_id from package__maintainer
+    );
 
     -- Package snapshot
     insert into snapshot (
