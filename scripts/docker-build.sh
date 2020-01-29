c85d91460c034160f9fcea6957aca77d8a85fd82
Sergio C. Arteaga <tegioz@icloud.com>
2020-01-29 14:35:32 +0100

    Setup Github actions
    Closes #28
    
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -3,3 +3,5 @@
 docker build -f cmd/hub/Dockerfile -t tegioz/hub .
 docker build -f cmd/chart-tracker/Dockerfile -t tegioz/chart-tracker .
 docker build -f database/migrations/Dockerfile -t tegioz/db-migrator .
+docker build -f database/tests/Dockerfile-db-tests -t tegioz/db-tests .
+docker build -f database/tests/Dockerfile-postgres-pgtap -t tegioz/postgres-pgtap .
