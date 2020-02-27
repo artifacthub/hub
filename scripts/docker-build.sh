b78ee4f7cac4180865b7433f3b64545dd02b486a
Sergio C. Arteaga <tegioz@icloud.com>
2020-02-27 22:49:11 +0100

    Include deployment in Github Actions CI workflow (#131)
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # Build docker images
-docker build -f cmd/hub/Dockerfile -t cncf/hub .
-docker build -f cmd/chart-tracker/Dockerfile -t cncf/chart-tracker .
-docker build -f database/migrations/Dockerfile -t cncf/db-migrator .
-docker build -f database/tests/Dockerfile-db-tests -t tegioz/db-tests .
-docker build -f database/tests/Dockerfile-postgres-pgtap -t tegioz/postgres-pgtap .
+GIT_SHA=$(git rev-parse HEAD)
+docker build -f cmd/hub/Dockerfile -t cncf/hub -t cncf/hub:$GIT_SHA .
+docker build -f cmd/chart-tracker/Dockerfile -t cncf/chart-tracker -t cncf/chart-tracker:$GIT_SHA .
+docker build -f database/migrations/Dockerfile -t cncf/db-migrator -t cncf/db-migrator:$GIT_SHA .
