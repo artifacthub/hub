d7e164a2c7f5f825bcde0cf2afadeae215cb4b59
Cynthia S. Garcia <cynthiasg@icloud.com>
2020-03-16 19:15:28 +0100

    Add pre-alpha disclaimer message (#184)
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

---
+++ web/src/layout/common/Disclaimer.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import classnames from 'classnames';
+import styles from './Disclaimer.module.css';
+
+const Disclaimer = () => {
+  const [openStatus, setOpenStatus] = useState(true);
+
+  return (
+    <div
+      className={classnames(
+        'alert alert-warning mb-0 py-2 px-3 rounded-0 text-center',
+        styles.disclaimer,
+        {'d-none': !openStatus},
+      )}
+      role="alert"
+    >
+      This is <strong>pre-alpha</strong> software and not for production use
+
+      <button
+        type="button"
+        className={`close position-absolute ${styles.close}`}
+        onClick={() => setOpenStatus(false)}
+      >
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  );
+}
+
+export default Disclaimer;
+
+
