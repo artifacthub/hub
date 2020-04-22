artifacthub/hub

commit 8705875d9c4ad798fd269f6a00baf126fc26b3f5

Add support for unregistering packages (#326)
Closes #322

Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

cmd/chart-tracker/worker.go
@@ -21,7 +21,8 @@ import (
 	"helm.sh/helm/v3/pkg/chart/loader"
 )
 
-// worker is in charge of handling jobs generated by the dispatcher.
+// worker is in charge of handling chart releases register and unregister jobs
+// generated by the dispatcher.
 type worker struct {
 	ctx        context.Context
 	id         int
@@ -53,39 +54,23 @@ func newWorker(
 	}
 }
 
-// run instructs the worker to start handling jobs. It will keep running until
-// the jobs queue is closed or the context is done.
-func (w *worker) run(wg *sync.WaitGroup, queue chan *job) {
-	defer wg.Done()
-	for {
-		select {
-		case j, ok := <-queue:
-			if !ok {
-				return
-			}
-			md := j.chartVersion.Metadata
-			w.logger.Debug().
-				Str("repo", j.repo.Name).
-				Str("chart", md.Name).
-				Str("version", md.Version).
-				Msg("Handling job")
-			if err := w.handleJob(j); err != nil {
-				w.logger.Error().
-					Err(err).
-					Str("repo", j.repo.Name).
-					Str("chart", md.Name).
-					Str("version", md.Version).
-					Msg("Error handling job")
-			}
-		case <-w.ctx.Done():
-			return
-		}
+// downloadImage downloads the image located at the url provided.
+func (w *worker) downloadImage(u string) ([]byte, error) {
+	resp, err := w.httpClient.Get(u)
+	if err != nil {
+		return nil, err
 	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusOK {
+		return ioutil.ReadAll(resp.Body)
+	}
+	return nil, fmt.Errorf("unexpected status code received: %d", resp.StatusCode)
 }
 
-// handleJob handles the provided job. This involves downloading the chart
-// archive, extracting its contents and register the corresponding package.
-func (w *worker) handleJob(j *job) error {
+// handleRegisterJob handles the provided chart release registration job. This
+// involves downloading the chart archive, extracting its contents and register
+// the corresponding package.
+func (w *worker) handleRegisterJob(j *job) error {
 	defer func() {
 		if r := recover(); r != nil {
 			w.logger.Error().
@@ -147,15 +132,15 @@ func (w *worker) handleJob(j *job) error {
 	p := &hub.Package{
 		Kind:            hub.Chart,
 		Name:            md.Name,
-		Description:     md.Description,
-		HomeURL:         md.Home,
 		LogoURL:         logoURL,
 		LogoImageID:     logoImageID,
+		Description:     md.Description,
 		Keywords:        md.Keywords,
-		Deprecated:      md.Deprecated,
+		HomeURL:         md.Home,
 		Version:         md.Version,
 		AppVersion:      md.AppVersion,
 		Digest:          j.chartVersion.Digest,
+		Deprecated:      md.Deprecated,
 		ChartRepository: j.repo,
 	}
 	readme := getFile(chart, "README.md")
@@ -186,6 +171,27 @@ func (w *worker) handleJob(j *job) error {
 	return err
 }
 
+// handleUnregisterJob handles the provided chart release unregistration job.
+// This involves deleting the package version corresponding to a given chart
+// release.
+func (w *worker) handleUnregisterJob(j *job) error {
+	// Unregister package
+	p := &hub.Package{
+		Kind:            hub.Chart,
+		Name:            j.chartVersion.Name,
+		Version:         j.chartVersion.Version,
+		ChartRepository: j.repo,
+	}
+	err := w.pkgManager.Unregister(w.ctx, p)
+	if err != nil {
+		w.ec.append(
+			j.repo.ChartRepositoryID,
+			fmt.Errorf("error unregistering package %s version %s: %w", p.Name, p.Version, err),
+		)
+	}
+	return err
+}
+
 // loadChart loads a chart from a remote archive located at the url provided.
 func (w *worker) loadChart(u string) (*chart.Chart, error) {
 	resp, err := w.httpClient.Get(u)
@@ -203,17 +209,43 @@ func (w *worker) loadChart(u string) (*chart.Chart, error) {
 	return nil, fmt.Errorf("unexpected status code received: %d", resp.StatusCode)
 }
 
-// downloadImage downloads the image located at the url provided.
-func (w *worker) downloadImage(u string) ([]byte, error) {
-	resp, err := w.httpClient.Get(u)
-	if err != nil {
-		return nil, err
+// run instructs the worker to start handling jobs. It will keep running until
+// the jobs queue is closed or the context is done.
+func (w *worker) run(wg *sync.WaitGroup, queue chan *job) {
+	defer wg.Done()
+	for {
+		select {
+		case j, ok := <-queue:
+			if !ok {
+				return
+			}
+			md := j.chartVersion.Metadata
+			w.logger.Debug().
+				Str("repo", j.repo.Name).
+				Str("chart", md.Name).
+				Str("version", md.Version).
+				Int("jobKind", int(j.kind)).
+				Msg("Handling job")
+			var err error
+			switch j.kind {
+			case register:
+				err = w.handleRegisterJob(j)
+			case unregister:
+				err = w.handleUnregisterJob(j)
+			}
+			if err != nil {
+				w.logger.Error().
+					Err(err).
+					Str("repo", j.repo.Name).
+					Str("chart", md.Name).
+					Str("version", md.Version).
+					Int("jobKind", int(j.kind)).
+					Msg("Error handling job")
+			}
+		case <-w.ctx.Done():
+			return
+		}
 	}
-	defer resp.Body.Close()
-	if resp.StatusCode == http.StatusOK {
-		return ioutil.ReadAll(resp.Body)
-	}
-	return nil, fmt.Errorf("unexpected status code received: %d", resp.StatusCode)
 }
 
 // getFile returns the file requested from the provided chart.

database/migrations/functions/packages/unregister_package.sql
@@ -1,0 +1,53 @@
+-- unregister_package unregisters the provided package version from the database.
+create or replace function unregister_package(p_pkg jsonb)
+returns void as $$
+declare
+    v_package_id uuid;
+    v_latest_version text;
+    v_snapshots_count int;
+    v_chart_repository_id text := (p_pkg->'chart_repository')->>'chart_repository_id';
+    v_semver_regexp text := '(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?';
+begin
+    -- Get package id
+    select p.package_id, p.latest_version, count(s.version)
+    into v_package_id, v_latest_version, v_snapshots_count
+    from package p
+    join snapshot s using (package_id)
+    where p.package_kind_id = (p_pkg->>'kind')::int
+    and p.name = p_pkg->>'name'
+    and chart_repository_id = nullif(v_chart_repository_id, '')::uuid
+    group by p.package_id, p.latest_version;
+    if not found then
+        return;
+    end if;
+
+    -- If the version to delete is the only one available we delete the package
+    -- (some other elements will be deleted on cascade)
+    if v_snapshots_count = 1 then
+        delete from package where package_id = v_package_id;
+
+        -- Clean up orphan maintainers not bound to any package
+        delete from maintainer where maintainer_id not in (
+            select maintainer_id from package__maintainer
+        );
+    else
+        -- If the version to delete is the last version, we need to update the
+        -- package's latest version
+        if p_pkg->>'version' = v_latest_version then
+            update package set latest_version = new_latest_version
+            from (
+                select version as new_latest_version
+                from snapshot
+                where package_id = v_package_id and version <> v_latest_version
+                order by
+                    (regexp_match(version, v_semver_regexp))[1:3]::int[] desc,
+                    (regexp_match(version, v_semver_regexp))[4] desc nulls first
+                limit 1
+            ) as nlv;
+        end if;
+
+        -- Delete version snapshot
+        delete from snapshot where package_id = v_package_id and version = p_pkg->>'version';
+    end if;
+end
+$$ language plpgsql;

database/tests/functions/packages/unregister_package.sql
@@ -1,0 +1,125 @@
+-- Start transaction and plan tests
+begin;
+select plan(10);
+
+-- Declare some variables
+\set user1ID '00000000-0000-0000-0000-000000000001'
+\set repo1ID '00000000-0000-0000-0000-000000000001'
+\set package1ID '00000000-0000-0000-0000-000000000001'
+\set image1ID '00000000-0000-0000-0000-000000000001'
+\set maintainer1ID '00000000-0000-0000-0000-000000000001'
+
+-- Seed some data
+insert into "user" (user_id, alias, email) values (:'user1ID', 'user1', 'user1@email.com');
+insert into chart_repository (chart_repository_id, name, display_name, url, user_id)
+values (:'repo1ID', 'repo1', 'Repo 1', 'https://repo1.com', :'user1ID');
+insert into package (
+    package_id,
+    name,
+    latest_version,
+    package_kind_id,
+    chart_repository_id
+) values (
+    :'package1ID',
+    'package1',
+    '1.0.0',
+    0,
+    :'repo1ID'
+);
+insert into snapshot (package_id, version) values (:'package1ID', '1.0.0');
+insert into snapshot (package_id, version) values (:'package1ID', '0.0.9');
+insert into snapshot (package_id, version) values (:'package1ID', '0.0.9-rc2');
+insert into snapshot (package_id, version) values (:'package1ID', '0.0.9-rc1');
+insert into maintainer (maintainer_id, name, email)
+values (:'maintainer1ID', 'name1', 'email1');
+insert into package__maintainer (package_id, maintainer_id)
+values (:'package1ID', :'maintainer1ID');
+
+-- Run some tests
+select unregister_package('
+{
+    "kind": 0,
+    "name": "package1",
+    "version": "1.0.0",
+    "chart_repository": {
+        "chart_repository_id": "00000000-0000-0000-0000-000000000001"
+    }
+}
+');
+select results_eq(
+    $$ select latest_version from package where name='package1' $$,
+    $$ values ('0.0.9') $$,
+    'Package last version should have been updated to 0.0.9'
+);
+select is_empty(
+    $$ select * from snapshot where version='1.0.0' $$,
+    'Package snapshot version 1.0.0 should have been deleted'
+);
+select unregister_package('
+{
+    "kind": 0,
+    "name": "package1",
+    "version": "0.0.9",
+    "chart_repository": {
+        "chart_repository_id": "00000000-0000-0000-0000-000000000001"
+    }
+}
+');
+select results_eq(
+    $$ select latest_version from package where name='package1' $$,
+    $$ values ('0.0.9-rc2') $$,
+    'Package last version should have been updated to 0.0.9-rc2'
+);
+select is_empty(
+    $$ select * from snapshot where version='0.0.9' $$,
+    'Package snapshot version 0.0.9 should have been deleted'
+);
+select unregister_package('
+{
+    "kind": 0,
+    "name": "package1",
+    "version": "0.0.9-rc1",
+    "chart_repository": {
+        "chart_repository_id": "00000000-0000-0000-0000-000000000001"
+    }
+}
+');
+select results_eq(
+    $$ select latest_version from package where name='package1' $$,
+    $$ values ('0.0.9-rc2') $$,
+    'Package last version should still be 0.0.9-rc2'
+);
+select is_empty(
+    $$ select * from snapshot where version='0.0.9-rc1' $$,
+    'Package snapshot version 0.0.9-rc1 should have been deleted'
+);
+select unregister_package('
+{
+    "kind": 0,
+    "name": "package1",
+    "version": "0.0.9-rc2",
+    "chart_repository": {
+        "chart_repository_id": "00000000-0000-0000-0000-000000000001"
+    }
+}
+');
+select is_empty(
+    $$ select * from package $$,
+    'Package should have been deleted'
+);
+select is_empty(
+    $$ select * from package $$,
+    'All package snapshots should have been deleted'
+);
+select is_empty(
+    $$ select * from package__maintainer $$,
+    'Package maintainer should have been deleted'
+);
+select is_empty(
+    $$ select * from maintainer $$,
+    'Orphan maintainer should have been deleted'
+);
+
+-- Finish tests and rollback transaction
+select * from finish();
+rollback;

cmd/chart-tracker/dispatcher.go
@@ -3,27 +3,42 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 	"sync"
 
 	"github.com/artifacthub/hub/internal/hub"
 	"github.com/rs/zerolog/log"
 	"golang.org/x/time/rate"
+	"helm.sh/helm/v3/pkg/chart"
 	"helm.sh/helm/v3/pkg/cli"
 	"helm.sh/helm/v3/pkg/getter"
 	"helm.sh/helm/v3/pkg/repo"
 )
 
-// job represents a job for processing a given chart version in the provided
-// repository. Jobs are created by the dispatcher that will eventually be
-// handled by a worker.
+// jobKind represents the kind of a job, which can be register or unregister.
+type jobKind int
+
+const (
+	// register represents a job to register a new chart release in the hub.
+	register jobKind = iota
+
+	// unregister represents a job to unregister an existing chart release from
+	// the hub.
+	unregister
+)
+
+// job represents a job for registering or unregistering a given chart release
+// available in the provided chart repository. Jobs are created by the
+// dispatcher and will eventually be handled by a worker.
 type job struct {
+	kind         jobKind
 	repo         *hub.ChartRepository
 	chartVersion *repo.ChartVersion
 	downloadLogo bool
 }
 
-// dispatcher is in charge of generating jobs and dispatching them among the
-// available workers.
+// dispatcher is in charge of generating jobs to register or unregister charts
+// releases and dispatching them among the available workers.
 type dispatcher struct {
 	ctx              context.Context
 	ec               *errorsCollector
@@ -50,7 +65,6 @@ func (d *dispatcher) run(wg *sync.WaitGroup, repos []*hub.ChartRepository) {
 	defer wg.Done()
 	defer close(d.Queue)
 
-	// Track repositories charts
 	var wgRepos sync.WaitGroup
 	limiter := rate.NewLimiter(25, 25)
 	for _, r := range repos {
@@ -59,16 +73,16 @@ func (d *dispatcher) run(wg *sync.WaitGroup, repos []*hub.ChartRepository) {
 			return
 		}
 		wgRepos.Add(1)
-		go d.trackRepositoryCharts(&wgRepos, r)
+		go d.syncRepositoryCharts(&wgRepos, r)
 	}
 
 	wgRepos.Wait()
 }
 
-// trackRepositoryCharts generates jobs for each of the chart versions found in
-// the given repository, provided that that version has not been already
-// processed and its digest has not changed.
-func (d *dispatcher) trackRepositoryCharts(wg *sync.WaitGroup, r *hub.ChartRepository) {
+// syncRepositoryCharts synchronizes the charts available in the chart
+// repository provided with the hub, generating jobs to register or unregister
+// charts releases as needed.
+func (d *dispatcher) syncRepositoryCharts(wg *sync.WaitGroup, r *hub.ChartRepository) {
 	defer wg.Done()
 
 	log.Info().Str("repo", r.Name).Msg("Loading chart repository index file")
@@ -79,12 +93,16 @@ func (d *dispatcher) trackRepositoryCharts(wg *sync.WaitGroup, r *hub.ChartRepos
 		log.Error().Err(err).Str("repo", r.Name).Msg(msg)
 		return
 	}
+
 	log.Info().Str("repo", r.Name).Msg("Loading registered packages digest")
-	packagesDigest, err := d.chartRepoManager.GetPackagesDigest(d.ctx, r.ChartRepositoryID)
+	registeredPackagesDigest, err := d.chartRepoManager.GetPackagesDigest(d.ctx, r.ChartRepositoryID)
 	if err != nil {
 		log.Error().Err(err).Str("repo", r.Name).Msg("Error getting repository packages digest")
 		return
 	}
+
+	// Register new or updated chart releases
+	chartsAvailable := make(map[string]struct{})
 	for _, chartVersions := range indexFile.Entries {
 		for i, chartVersion := range chartVersions {
 			var downloadLogo bool
@@ -92,8 +110,10 @@ func (d *dispatcher) trackRepositoryCharts(wg *sync.WaitGroup, r *hub.ChartRepos
 				downloadLogo = true
 			}
 			key := fmt.Sprintf("%s@%s", chartVersion.Metadata.Name, chartVersion.Metadata.Version)
-			if chartVersion.Digest != packagesDigest[key] {
+			chartsAvailable[key] = struct{}{}
+			if chartVersion.Digest != registeredPackagesDigest[key] {
 				d.Queue <- &job{
+					kind:         register,
 					repo:         r,
 					chartVersion: chartVersion,
 					downloadLogo: downloadLogo,
@@ -104,6 +124,30 @@ func (d *dispatcher) trackRepositoryCharts(wg *sync.WaitGroup, r *hub.ChartRepos
 				return
 			default:
 			}
+		}
+	}
+
+	// Unregister chart releases no longer available in the repository
+	for key := range registeredPackagesDigest {
+		if _, ok := chartsAvailable[key]; !ok {
+			p := strings.Split(key, "@")
+			name := p[0]
+			version := p[1]
+			d.Queue <- &job{
+				kind: unregister,
+				repo: r,
+				chartVersion: &repo.ChartVersion{
+					Metadata: &chart.Metadata{
+						Name:    name,
+						Version: version,
+					},
+				},
+			}
+		}
+		select {
+		case <-d.ctx.Done():
+			return
+		default:
 		}
 	}
 }

cmd/hub/main.go
@@ -54,7 +54,7 @@ func main() {
 	srv := &http.Server{
 		Addr:         addr,
 		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 5 * time.Second,
+		WriteTimeout: 30 * time.Second,
 		IdleTimeout:  1 * time.Minute,
 		Handler:      handlers.Setup(cfg, svc).Router,
 	}

cmd/securityhub-poc/main.go
@@ -123,9 +123,9 @@ func (r *SecurityHubRegistrar) registerPackage(orgID, basePath, pkgPath string) 
 	// Build package to register
 	p := &hub.Package{
 		Name:           e.Name,
-		Description:    e.ShortDescription,
 		LogoURL:        logoURL,
 		LogoImageID:    logoImageID,
+		Description:    e.ShortDescription,
 		Keywords:       e.Keywords,
 		Version:        e.Version,
 		Readme:         e.Description,

database/migrations/functions/001_load_functions.sql
@@ -15,6 +15,7 @@
 {{ template "users/update_user_profile.sql" }}
 {{ template "users/verify_email.sql" }}
 
+{{ template "packages/generate_package_tsdoc.sql" }}
 {{ template "packages/get_package.sql" }}
 {{ template "packages/get_packages_starred_by_user.sql" }}
 {{ template "packages/get_packages_stats.sql" }}
@@ -23,6 +24,7 @@
 {{ template "packages/search_packages.sql" }}
 {{ template "packages/semver_gte.sql" }}
 {{ template "packages/toggle_star.sql" }}
+{{ template "packages/unregister_package.sql" }}
 
 {{ template "chart_repositories/add_chart_repository.sql" }}
 {{ template "chart_repositories/delete_chart_repository.sql" }}

database/migrations/functions/packages/generate_package_tsdoc.sql
@@ -1,0 +1,14 @@
+-- generate_package_tsdoc generates the package's document used to perform full
+-- text searches.
+create or replace function generate_package_tsdoc(
+    p_name text,
+    p_display_name text,
+    p_description text,
+    p_keywords text[]
+) returns tsvector as $$
+    select
+        setweight(to_tsvector(p_name), 'A') ||
+        setweight(to_tsvector(coalesce(p_display_name, '')), 'A') ||
+        setweight(to_tsvector(coalesce(p_description, '')), 'B') ||
+        setweight(to_tsvector(array_to_string(coalesce(p_keywords, '{}'), ' ')), 'C');
+$$ language sql immutable;

commit 08772d0af9b882601327bfa510e47aedfe633598

Fix bug introduced in #326
Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

cmd/chart-tracker/worker.go
@@ -13,6 +13,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/Masterminds/semver/v3"
 	"github.com/artifacthub/hub/internal/hub"
 	"github.com/artifacthub/hub/internal/img"
 	"github.com/rs/zerolog"
@@ -109,6 +110,18 @@ func (w *worker) handleRegisterJob(j *job) error {
 		return nil
 	}
 	md := chart.Metadata
+
+	// Validate chart version
+	if _, err := semver.StrictNewVersion(md.Version); err != nil {
+		w.ec.append(j.repo.ChartRepositoryID, fmt.Errorf("invalid chart version %s: %w", md.Version, err))
+		w.logger.Error().
+			Str("repo", j.repo.Name).
+			Str("chart", md.Name).
+			Str("version", md.Version).
+			Err(err).
+			Msg("Invalid chart version")
+		return err
+	}
 
 	// Store chart logo when available if requested
 	var logoURL, logoImageID string

database/migrations/functions/packages/unregister_package.sql
@@ -43,7 +43,8 @@ begin
                     (regexp_match(version, v_semver_regexp))[1:3]::int[] desc,
                     (regexp_match(version, v_semver_regexp))[4] desc nulls first
                 limit 1
-            ) as nlv;
+            ) as nlv
+            where package_id = v_package_id;
         end if;
 
         -- Delete version snapshot

go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/MakeNowJust/heredoc v1.0.0 // indirect
+	github.com/Masterminds/semver/v3 v3.1.0
 	github.com/disintegration/imaging v1.6.2
 	github.com/docker/spdystream v0.0.0-20181023171402-6480d4af844c // indirect
 	github.com/domodwyer/mailyak v3.1.1+incompatible

go.sum
@@ -23,6 +23,8 @@ github.com/MakeNowJust/heredoc v1.0.0/go.mod h1:mG5amYoWBHf8vpLOuehzbGGw0EHxpZZ6
 github.com/Masterminds/goutils v1.1.0/go.mod h1:8cTjp+g8YejhMuvIA5y2vz3BpJxksy863GQaJW2MFNU=
 github.com/Masterminds/semver/v3 v3.0.3 h1:znjIyLfpXEDQjOIEWh+ehwpTU14UzUPub3c3sm36u14=
 github.com/Masterminds/semver/v3 v3.0.3/go.mod h1:VPu/7SZ7ePZ3QOrcuXROw5FAcLl4a0cBrbBpGY/8hQs=
+github.com/Masterminds/semver/v3 v3.1.0 h1:Y2lUDsFKVRSYGojLJ1yLxSXdMmMYTYls0rCvoqmMUQk=
+github.com/Masterminds/semver/v3 v3.1.0/go.mod h1:VPu/7SZ7ePZ3QOrcuXROw5FAcLl4a0cBrbBpGY/8hQs=
 github.com/Masterminds/sprig/v3 v3.0.2/go.mod h1:oesJ8kPONMONaZgtiHNzUShJbksypC5kWczhZAf6+aU=
 github.com/Masterminds/vcs v1.13.1/go.mod h1:N09YCmOQr6RLxC6UNHzuVwAdodYbbnycGHSmwVJjcKA=
 github.com/Microsoft/go-winio v0.4.15-0.20190919025122-fc70bd9a86b5/go.mod h1:tTuCMEN+UleMWgg9dVx4Hu52b1bJo+59jBh3ajtinzw=