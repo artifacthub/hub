0cb788f16cefbf1e83b021796c963696d1e53829
Cintia Sanchez Garcia <cynthiasg@icloud.com>
2020-04-24 18:29:53 +0200

    Add web analytics
    Closes #330
    
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

---
+++ web/src/analytics/analytics.ts
@@ -0,0 +1,31 @@
+//@ts-ignore
+import googleAnalytics from '@analytics/google-analytics';
+import Analytics from 'analytics';
+import isUndefined from 'lodash/isUndefined';
+
+const getPlugins = (): object[] | undefined => {
+  let plugins: object[] = [];
+  const analyticsConfig = (window as any).analyticsConfig;
+  if (!isUndefined(analyticsConfig)) {
+    if (
+      analyticsConfig.hasOwnProperty('gaTrackingID') &&
+      analyticsConfig.gaTrackingID !== '' &&
+      analyticsConfig.gaTrackingID !== '{{ .gaTrackingID }}'
+    ) {
+      plugins.push(
+        googleAnalytics({
+          trackingId: analyticsConfig.gaTrackingID,
+        })
+      );
+    }
+  }
+
+  return plugins;
+};
+
+const analytics = Analytics({
+  app: 'ArtifactHub',
+  plugins: getPlugins(),
+});
+
+export default analytics;
--- web/src/utils/history.ts
+++ web/src/utils/history.ts
@@ -1,3 +1,10 @@
 import { createBrowserHistory } from 'history';
+
+import analytics from '../analytics/analytics';
 const history = createBrowserHistory();
+
+history.listen((location) => {
+  analytics.page();
+});
+
 export default history;
