cea7201b818f9e407d6b2b4e15d8def463d9f9ab
Sergio C. Arteaga <tegioz@icloud.com>
2020-04-24 17:41:53 +0200

    Improve input validation for packages operations (#337)
    Related to #333
    
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- internal/pkg/manager.go
+++ internal/pkg/manager.go
@@ -4,13 +4,21 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 
+	"github.com/Masterminds/semver/v3"
 	"github.com/artifacthub/hub/internal/hub"
 	"github.com/jackc/pgx/v4"
+	"github.com/satori/uuid"
 )
 
-// ErrNotFound indicates that the package requested was not found.
-var ErrNotFound = errors.New("package not found")
+var (
+	// ErrInvalidInput indicates that the input provided is not valid.
+	ErrInvalidInput = errors.New("invalid input")
+
+	// ErrNotFound indicates that the package requested was not found.
+	ErrNotFound = errors.New("package not found")
+)
 
 // Manager provides an API to manage packages.
 type Manager struct {
@@ -27,6 +35,12 @@ func NewManager(db hub.DB) *Manager {
 // GetJSON returns the package identified by the input provided as a json
 // object. The json object is built by the database.
 func (m *Manager) GetJSON(ctx context.Context, input *hub.GetPackageInput) ([]byte, error) {
+	// Validate input
+	if input.PackageName == "" {
+		return nil, fmt.Errorf("%w: %s", ErrInvalidInput, "package name not provided")
+	}
+
+	// Get package from database
 	query := "select get_package($1::uuid, $2::jsonb)"
 	userID := getUserID(ctx)
 	inputJSON, _ := json.Marshal(input)
@@ -62,6 +76,66 @@ func (m *Manager) GetUpdatesJSON(ctx context.Context) ([]byte, error) {
 
 // Register registers the package provided in the database.
 func (m *Manager) Register(ctx context.Context, pkg *hub.Package) error {
+	// Validate input
+	if !isValidKind(pkg.Kind) {
+		return fmt.Errorf("%w: %s", ErrInvalidInput, "invalid kind")
+	}
+	if pkg.Name == "" {
+		return fmt.Errorf("%w: %s", ErrInvalidInput, "name not provided")
+	}
+	if pkg.Version == "" {
+		return fmt.Errorf("%w: %s", ErrInvalidInput, "version not provided")
+	}
+	if _, err := semver.StrictNewVersion(pkg.Version); err != nil {
+		return fmt.Errorf("%w: %s", ErrInvalidInput, "invalid version (semantic version expected)")
+	}
+	if pkg.Kind == hub.Chart {
+		if pkg.ChartRepository == nil {
+			return fmt.Errorf("%w: %s", ErrInvalidInput, "chart repository not provided")
+		}
+		if pkg.ChartRepository.ChartRepositoryID == "" {
+			return fmt.Errorf("%w: %s", ErrInvalidInput, "chart repository id not provided")
+		}
+		if _, err := uuid.FromString(pkg.ChartRepository.ChartRepositoryID); err != nil {
+			return fmt.Errorf("%w: %s", ErrInvalidInput, "invalid chart repository id")
+		}
+		if pkg.UserID != "" {
+			return fmt.Errorf("%w: %s", ErrInvalidInput, "unexpected user id provided")
+		}
+		if pkg.OrganizationID != "" {
+			return fmt.Errorf("%w: %s", ErrInvalidInput, "unexpected organization id provided")
+		}
+	} else {
+		if pkg.UserID == "" && pkg.OrganizationID == "" {
+			return fmt.Errorf("%w: %s", ErrInvalidInput, "user id or organization id not provided")
+		}
+		if pkg.UserID != "" && pkg.OrganizationID != "" {
+			return fmt.Errorf("%w: %s", ErrInvalidInput, "both user id and organization id provided")
+		}
+		if pkg.UserID != "" {
+			if _, err := uuid.FromString(pkg.UserID); err != nil {
+				return fmt.Errorf("%w: %s", ErrInvalidInput, "invalid user id")
+			}
+		}
+		if pkg.OrganizationID != "" {
+			if _, err := uuid.FromString(pkg.OrganizationID); err != nil {
+				return fmt.Errorf("%w: %s", ErrInvalidInput, "invalid organization id")
+			}
+		}
+		if pkg.ChartRepository != nil {
+			return fmt.Errorf("%w: %s", ErrInvalidInput, "unexpected chart repository provided")
+		}
+	}
+	for _, m := range pkg.Maintainers {
+		if m.Name == "" {
+			return fmt.Errorf("%w: %s", ErrInvalidInput, "maintainer name not provided")
+		}
+		if m.Email == "" {
+			return fmt.Errorf("%w: %s", ErrInvalidInput, "maintainer email not provided")
+		}
+	}
+
+	// Register package in database
 	pkgJSON, _ := json.Marshal(pkg)
 	_, err := m.db.Exec(ctx, "select register_package($1::jsonb)", pkgJSON)
 	return err
@@ -70,19 +144,68 @@ func (m *Manager) Register(ctx context.Context, pkg *hub.Package) error {
 // SearchJSON returns a json object with the search results produced by the
 // input provided. The json object is built by the database.
 func (m *Manager) SearchJSON(ctx context.Context, input *hub.SearchPackageInput) ([]byte, error) {
+	// Validate input
+	if input.Limit <= 0 || input.Limit > 50 {
+		return nil, fmt.Errorf("%w: %s", ErrInvalidInput, "invalid limit (0 < l <= 50)")
+	}
+	if input.Offset < 0 {
+		return nil, fmt.Errorf("%w: %s", ErrInvalidInput, "invalid offset (o >= 0)")
+	}
+	for _, alias := range input.Users {
+		if alias == "" {
+			return nil, fmt.Errorf("%w: %s", ErrInvalidInput, "invalid user alias")
+		}
+	}
+	for _, name := range input.Orgs {
+		if name == "" {
+			return nil, fmt.Errorf("%w: %s", ErrInvalidInput, "invalid organization name")
+		}
+	}
+	for _, name := range input.ChartRepositories {
+		if name == "" {
+			return nil, fmt.Errorf("%w: %s", ErrInvalidInput, "invalid chart repository name")
+		}
+	}
+
+	// Search packages in database
 	inputJSON, _ := json.Marshal(input)
 	return m.dbQueryJSON(ctx, "select search_packages($1::jsonb)", inputJSON)
 }
 
-// ToggleStar stars on unstars a given package for the provided user.
+// ToggleStar stars or unstars a given package for the provided user.
 func (m *Manager) ToggleStar(ctx context.Context, packageID string) error {
 	userID := ctx.Value(hub.UserIDKey).(string)
+
+	// Validate input
+	if packageID == "" {
+		return fmt.Errorf("%w: %s", ErrInvalidInput, "package id not provided")
+	}
+	if _, err := uuid.FromString(packageID); err != nil {
+		return fmt.Errorf("%w: %s", ErrInvalidInput, "invalid package id")
+	}
+
+	// Toggle star in database
 	_, err := m.db.Exec(ctx, "select toggle_star($1::uuid, $2::uuid)", userID, packageID)
 	return err
 }
 
 // Unregister unregisters the package provided from the database.
 func (m *Manager) Unregister(ctx context.Context, pkg *hub.Package) error {
+	// Validate input
+	if !isValidKind(pkg.Kind) {
+		return fmt.Errorf("%w: %s", ErrInvalidInput, "invalid kind")
+	}
+	if pkg.Name == "" {
+		return fmt.Errorf("%w: %s", ErrInvalidInput, "name not provided")
+	}
+	if pkg.Version == "" {
+		return fmt.Errorf("%w: %s", ErrInvalidInput, "version not provided")
+	}
+	if _, err := semver.StrictNewVersion(pkg.Version); err != nil {
+		return fmt.Errorf("%w: %s", ErrInvalidInput, "invalid version (semantic version expected)")
+	}
+
+	// Unregister package from database
 	pkgJSON, _ := json.Marshal(pkg)
 	_, err := m.db.Exec(ctx, "select unregister_package($1::jsonb)", pkgJSON)
 	return err
@@ -106,4 +229,14 @@ func getUserID(ctx context.Context) *string {
 		userID = &v
 	}
 	return userID
+}
+
+// isValidKind checks if the provided package kind is valid.
+func isValidKind(kind hub.PackageKind) bool {
+	for _, validKind := range []hub.PackageKind{hub.Chart, hub.Falco, hub.OPA} {
+		if kind == validKind {
+			return true
+		}
+	}
+	return false
 }
--- internal/pkg/manager_test.go
+++ internal/pkg/manager_test.go
@@ -2,6 +2,7 @@ package pkg
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/artifacthub/hub/internal/hub"
@@ -14,12 +15,18 @@ func TestGetJSON(t *testing.T) {
 	dbQuery := "select get_package($1::uuid, $2::jsonb)"
 	ctx := context.WithValue(context.Background(), hub.UserIDKey, "userID")
 
+	t.Run("invalid input", func(t *testing.T) {
+		m := NewManager(nil)
+		_, err := m.GetJSON(ctx, &hub.GetPackageInput{})
+		assert.True(t, errors.Is(err, ErrInvalidInput))
+	})
+
 	t.Run("database query succeeded", func(t *testing.T) {
 		db := &tests.DBMock{}
 		db.On("QueryRow", dbQuery, mock.Anything, mock.Anything).Return([]byte("dataJSON"), nil)
 		m := NewManager(db)
 
-		dataJSON, err := m.GetJSON(ctx, &hub.GetPackageInput{})
+		dataJSON, err := m.GetJSON(ctx, &hub.GetPackageInput{PackageName: "pkg1"})
 		assert.NoError(t, err)
 		assert.Equal(t, []byte("dataJSON"), dataJSON)
 		db.AssertExpectations(t)
@@ -30,7 +37,7 @@ func TestGetJSON(t *testing.T) {
 		db.On("QueryRow", dbQuery, mock.Anything, mock.Anything).Return(nil, tests.ErrFakeDatabaseFailure)
 		m := NewManager(db)
 
-		dataJSON, err := m.GetJSON(context.Background(), &hub.GetPackageInput{})
+		dataJSON, err := m.GetJSON(context.Background(), &hub.GetPackageInput{PackageName: "pkg1"})
 		assert.Equal(t, tests.ErrFakeDatabaseFailure, err)
 		assert.Nil(t, dataJSON)
 		db.AssertExpectations(t)
@@ -158,6 +165,184 @@ func TestRegister(t *testing.T) {
 		},
 	}
 
+	t.Run("invalid input", func(t *testing.T) {
+		testCases := []struct {
+			errMsg string
+			p      *hub.Package
+		}{
+			{
+				"invalid kind",
+				&hub.Package{
+					Kind: hub.PackageKind(99),
+				},
+			},
+			{
+				"name not provided",
+				&hub.Package{
+					Kind: hub.Chart,
+				},
+			},
+			{
+				"version not provided",
+				&hub.Package{
+					Kind: hub.Chart,
+					Name: "package1",
+				},
+			},
+			{
+				"invalid version (semantic version expected)",
+				&hub.Package{
+					Kind:    hub.Chart,
+					Name:    "package1",
+					Version: "1.0",
+				},
+			},
+			{
+				"chart repository not provided",
+				&hub.Package{
+					Kind:    hub.Chart,
+					Name:    "package1",
+					Version: "1.0.0",
+				},
+			},
+			{
+				"chart repository id not provided",
+				&hub.Package{
+					Kind:    hub.Chart,
+					Name:    "package1",
+					Version: "1.0.0",
+					ChartRepository: &hub.ChartRepository{
+						ChartRepositoryID: "",
+					},
+				},
+			},
+			{
+				"invalid chart repository id",
+				&hub.Package{
+					Kind:    hub.Chart,
+					Name:    "package1",
+					Version: "1.0.0",
+					ChartRepository: &hub.ChartRepository{
+						ChartRepositoryID: "invalid",
+					},
+				},
+			},
+			{
+				"unexpected user id provided",
+				&hub.Package{
+					Kind:    hub.Chart,
+					Name:    "package1",
+					Version: "1.0.0",
+					ChartRepository: &hub.ChartRepository{
+						ChartRepositoryID: "00000000-0000-0000-0000-000000000001",
+					},
+					UserID: "unexpected",
+				},
+			},
+			{
+				"unexpected organization id provided",
+				&hub.Package{
+					Kind:    hub.Chart,
+					Name:    "package1",
+					Version: "1.0.0",
+					ChartRepository: &hub.ChartRepository{
+						ChartRepositoryID: "00000000-0000-0000-0000-000000000001",
+					},
+					OrganizationID: "unexpected",
+				},
+			},
+			{
+				"user id or organization id not provided",
+				&hub.Package{
+					Kind:    hub.Falco,
+					Name:    "package1",
+					Version: "1.0.0",
+				},
+			},
+			{
+				"both user id and organization id provided",
+				&hub.Package{
+					Kind:           hub.Falco,
+					Name:           "package1",
+					Version:        "1.0.0",
+					UserID:         "unexpected",
+					OrganizationID: "unexpected",
+				},
+			},
+			{
+				"invalid user id",
+				&hub.Package{
+					Kind:    hub.Falco,
+					Name:    "package1",
+					Version: "1.0.0",
+					UserID:  "invalid",
+				},
+			},
+			{
+				"invalid organization id",
+				&hub.Package{
+					Kind:           hub.Falco,
+					Name:           "package1",
+					Version:        "1.0.0",
+					OrganizationID: "invalid",
+				},
+			},
+			{
+				"unexpected chart repository provided",
+				&hub.Package{
+					Kind:           hub.Falco,
+					Name:           "package1",
+					Version:        "1.0.0",
+					OrganizationID: "00000000-0000-0000-0000-000000000001",
+					ChartRepository: &hub.ChartRepository{
+						ChartRepositoryID: "unexpected",
+					},
+				},
+			},
+			{
+				"maintainer name not provided",
+				&hub.Package{
+					Kind:    hub.Chart,
+					Name:    "package1",
+					Version: "1.0.0",
+					ChartRepository: &hub.ChartRepository{
+						ChartRepositoryID: "00000000-0000-0000-0000-000000000001",
+					},
+					Maintainers: []*hub.Maintainer{
+						{
+							Email: "email",
+						},
+					},
+				},
+			},
+			{
+				"maintainer email not provided",
+				&hub.Package{
+					Kind:    hub.Chart,
+					Name:    "package1",
+					Version: "1.0.0",
+					ChartRepository: &hub.ChartRepository{
+						ChartRepositoryID: "00000000-0000-0000-0000-000000000001",
+					},
+					Maintainers: []*hub.Maintainer{
+						{
+							Name: "name",
+						},
+					},
+				},
+			},
+		}
+		for _, tc := range testCases {
+			tc := tc
+			t.Run(tc.errMsg, func(t *testing.T) {
+				m := NewManager(nil)
+				err := m.Register(context.Background(), tc.p)
+				assert.True(t, errors.Is(err, ErrInvalidInput))
+				assert.Contains(t, err.Error(), tc.errMsg)
+			})
+		}
+	})
+
 	t.Run("successful package registration", func(t *testing.T) {
 		db := &tests.DBMock{}
 		db.On("Exec", dbQuery, mock.Anything).Return(nil)
@@ -181,7 +366,74 @@ func TestRegister(t *testing.T) {
 
 func TestSearchJSON(t *testing.T) {
 	dbQuery := "select search_packages($1::jsonb)"
-	input := &hub.SearchPackageInput{Text: "kw1"}
+	input := &hub.SearchPackageInput{
+		Limit: 10,
+		Text:  "kw1",
+	}
+
+	t.Run("invalid input", func(t *testing.T) {
+		testCases := []struct {
+			errMsg string
+			input  *hub.SearchPackageInput
+		}{
+			{
+				"invalid limit (0 < l <= 50)",
+				&hub.SearchPackageInput{
+					Limit: -1,
+				},
+			},
+			{
+				"invalid limit (0 < l <= 50)",
+				&hub.SearchPackageInput{
+					Limit: 0,
+				},
+			},
+			{
+				"invalid limit (0 < l <= 50)",
+				&hub.SearchPackageInput{
+					Limit: 100,
+				},
+			},
+			{
+				"invalid offset (o >= 0)",
+				&hub.SearchPackageInput{
+					Limit:  10,
+					Offset: -1,
+				},
+			},
+			{
+				"invalid user alias",
+				&hub.SearchPackageInput{
+					Limit: 10,
+					Users: []string{""},
+				},
+			},
+			{
+				"invalid organization name",
+				&hub.SearchPackageInput{
+					Limit: 10,
+					Orgs:  []string{""},
+				},
+			},
+			{
+				"invalid chart repository name",
+				&hub.SearchPackageInput{
+					Limit:             10,
+					ChartRepositories: []string{""},
+				},
+			},
+		}
+		for _, tc := range testCases {
+			tc := tc
+			t.Run(tc.errMsg, func(t *testing.T) {
+				m := NewManager(nil)
+				dataJSON, err := m.SearchJSON(context.Background(), tc.input)
+				assert.True(t, errors.Is(err, ErrInvalidInput))
+				assert.Contains(t, err.Error(), tc.errMsg)
+				assert.Nil(t, dataJSON)
+			})
+		}
+	})
 
 	t.Run("database query succeeded", func(t *testing.T) {
 		db := &tests.DBMock{}
@@ -209,6 +461,7 @@ func TestSearchJSON(t *testing.T) {
 func TestToggleStar(t *testing.T) {
 	dbQuery := "select toggle_star($1::uuid, $2::uuid)"
 	ctx := context.WithValue(context.Background(), hub.UserIDKey, "userID")
+	pkgID := "00000000-0000-0000-0000-000000000001"
 
 	t.Run("user id not found in ctx", func(t *testing.T) {
 		m := NewManager(nil)
@@ -217,22 +470,41 @@ func TestToggleStar(t *testing.T) {
 		})
 	})
 
+	t.Run("invalid input", func(t *testing.T) {
+		testCases := []struct {
+			errMsg    string
+			packageID string
+		}{
+			{"package id not provided", ""},
+			{"invalid package id", "pkgID"},
+		}
+		for _, tc := range testCases {
+			tc := tc
+			t.Run(tc.errMsg, func(t *testing.T) {
+				m := NewManager(nil)
+				err := m.ToggleStar(ctx, tc.packageID)
+				assert.True(t, errors.Is(err, ErrInvalidInput))
+				assert.Contains(t, err.Error(), tc.errMsg)
+			})
+		}
+	})
+
 	t.Run("database query succeeded", func(t *testing.T) {
 		db := &tests.DBMock{}
-		db.On("Exec", dbQuery, "userID", "pkgID").Return(nil)
+		db.On("Exec", dbQuery, "userID", pkgID).Return(nil)
 		m := NewManager(db)
 
-		err := m.ToggleStar(ctx, "pkgID")
+		err := m.ToggleStar(ctx, pkgID)
 		assert.NoError(t, err)
 		db.AssertExpectations(t)
 	})
 
 	t.Run("database error", func(t *testing.T) {
 		db := &tests.DBMock{}
-		db.On("Exec", dbQuery, "userID", "pkgID").Return(tests.ErrFakeDatabaseFailure)
+		db.On("Exec", dbQuery, "userID", pkgID).Return(tests.ErrFakeDatabaseFailure)
 		m := NewManager(db)
 
-		err := m.ToggleStar(ctx, "pkgID")
+		err := m.ToggleStar(ctx, pkgID)
 		assert.Equal(t, tests.ErrFakeDatabaseFailure, err)
 		db.AssertExpectations(t)
 	})
@@ -249,6 +521,50 @@ func TestUnregister(t *testing.T) {
 			ChartRepositoryID: "00000000-0000-0000-0000-000000000001",
 		},
 	}
+
+	t.Run("invalid input", func(t *testing.T) {
+		testCases := []struct {
+			errMsg string
+			p      *hub.Package
+		}{
+			{
+				"invalid kind",
+				&hub.Package{
+					Kind: hub.PackageKind(99),
+				},
+			},
+			{
+				"name not provided",
+				&hub.Package{
+					Kind: hub.Chart,
+				},
+			},
+			{
+				"version not provided",
+				&hub.Package{
+					Kind: hub.Chart,
+					Name: "package1",
+				},
+			},
+			{
+				"invalid version (semantic version expected)",
+				&hub.Package{
+					Kind:    hub.Chart,
+					Name:    "package1",
+					Version: "1.0",
+				},
+			},
+		}
+		for _, tc := range testCases {
+			tc := tc
+			t.Run(tc.errMsg, func(t *testing.T) {
+				m := NewManager(nil)
+				err := m.Unregister(context.Background(), tc.p)
+				assert.True(t, errors.Is(err, ErrInvalidInput))
+				assert.Contains(t, err.Error(), tc.errMsg)
+			})
+		}
+	})
 
 	t.Run("successful package unregistration", func(t *testing.T) {
 		db := &tests.DBMock{}
