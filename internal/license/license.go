e39cd7181c3932ef21c20d16e3644a37652b3d67
Sergio C. Arteaga <tegioz@icloud.com>
2020-05-26 11:31:53 +0200

    Fix issue detecting license (#399)
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- internal/license/license.go
+++ internal/license/license.go
@@ -14,6 +14,7 @@ func Detect(data []byte) string {
 		for name, match := range matches {
 			if match.Confidence > highestConfidence {
 				license = name
+				highestConfidence = match.Confidence
 			}
 		}
 	}
