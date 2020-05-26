0c88efbf01eb3d9b548d9d10e526beceac296b93
Sergio Castaño Arteaga <tegioz@icloud.com>
2020-05-26 14:08:55 +0200

    Sort webhooks list by name
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- database/migrations/functions/webhooks/get_user_webhooks.sql
+++ database/migrations/functions/webhooks/get_user_webhooks.sql
@@ -1,8 +1,12 @@
 -- get_user_webhooks returns the webhooks that belong to the requesting user.
 create or replace function get_user_webhooks(p_user_id uuid)
 returns setof json as $$
-    select coalesce(json_agg(wh), '[]')
-    from webhook
-    cross join get_webhook(null::uuid, webhook_id) as wh
-    where user_id = p_user_id;
+    select coalesce(json_agg(whJSON), '[]')
+    from (
+        select whJSON
+        from webhook wh
+        cross join get_webhook(null::uuid, webhook_id) as whJSON
+        where user_id = p_user_id
+        order by wh.name asc
+    ) whs;
 $$ language sql;
