24a1dd838e48bc3107080ae842160202f94f60f5
Sergio C. Arteaga <tegioz@icloud.com>
2020-05-26 14:15:48 +0200

    Sort webhooks list by name (#400)
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- database/migrations/functions/webhooks/get_org_webhooks.sql
+++ database/migrations/functions/webhooks/get_org_webhooks.sql
@@ -2,12 +2,16 @@
 -- provided if the requesting user belongs to it.
 create or replace function get_org_webhooks(p_user_id uuid, p_org_name text)
 returns setof json as $$
-    select coalesce(json_agg(wh), '[]')
-    from webhook
-    cross join get_webhook(null::uuid, webhook_id) as wh
-    join organization o using (organization_id)
-    join user__organization uo using (organization_id)
-    where o.name = p_org_name
-    and uo.user_id = p_user_id
-    and uo.confirmed = true;
+    select coalesce(json_agg(whJSON), '[]')
+    from (
+        select whJSON
+        from webhook wh
+        cross join get_webhook(null::uuid, webhook_id) as whJSON
+        join organization o using (organization_id)
+        join user__organization uo using (organization_id)
+        where o.name = p_org_name
+        and uo.user_id = p_user_id
+        and uo.confirmed = true
+        order by wh.name asc
+    ) whs;
 $$ language sql;
