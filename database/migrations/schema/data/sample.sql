8015eef0aec5c7cb670ce1797bbf6e04d99eda09
Sergio Castaño Arteaga <tegioz@icloud.com>
2020-06-03 12:51:22 +0200

    Setup Artifact Hub Helm chart repo
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- database/migrations/schema/data/sample.sql
+++ database/migrations/schema/data/sample.sql
@@ -1,6 +1,6 @@
 -- Register demo user
 insert into "user" (user_id, alias, email, email_verified, password)
-values ('00000000-0000-0000-0000-000000000001', 'demo', 'demo@artifacthub.com', true, '$2a$10$FRAFMqDgJYgKEIrW8Y3Pqu7m5uFjtGxNAjlOtXA1iiFGGH7NycLIO');
+values ('00000000-0000-0000-0000-000000000001', 'demo', 'demo@artifacthub.io', true, '$2a$10$FRAFMqDgJYgKEIrW8Y3Pqu7m5uFjtGxNAjlOtXA1iiFGGH7NycLIO');
 
 -- Register Helm repositories
 insert into chart_repository (name, url, user_id)
