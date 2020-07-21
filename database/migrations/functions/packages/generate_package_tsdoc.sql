5c4d4339f2c0dbf9dfa63c0800ad6f410f9862b3
Sergio C. Arteaga <tegioz@icloud.com>
2020-07-21 20:54:14 +0200

    Include repo and publisher name in pkg ts doc (#543)
    This will allow searching by the name of the repository, name of the owning organization or user as well as by the provider name available in OLM operators.
    
    Closes #536
    
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- database/migrations/functions/packages/generate_package_tsdoc.sql
+++ database/migrations/functions/packages/generate_package_tsdoc.sql
@@ -4,11 +4,15 @@ create or replace function generate_package_tsdoc(
     p_name text,
     p_display_name text,
     p_description text,
-    p_keywords text[]
+    p_keywords text[],
+    p_repository text[],
+    p_publisher text[]
 ) returns tsvector as $$
     select
         setweight(to_tsvector(p_name), 'A') ||
         setweight(to_tsvector(coalesce(p_display_name, '')), 'A') ||
         setweight(to_tsvector(coalesce(p_description, '')), 'B') ||
-        setweight(to_tsvector(array_to_string(coalesce(p_keywords, '{}'), ' ')), 'C');
+        setweight(to_tsvector(array_to_string(coalesce(p_keywords, '{}'), ' ')), 'C') ||
+        setweight(to_tsvector(array_to_string(coalesce(p_repository, '{}'), ' ')), 'B') ||
+        setweight(to_tsvector(array_to_string(coalesce(p_publisher, '{}'), ' ')), 'B');
 $$ language sql immutable;
