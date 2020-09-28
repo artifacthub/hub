1fdf18742fd2cf307f8520f46de14af6cfc50044
Cynthia S. Garcia <cynthiasg@icloud.com>
2020-09-28 13:04:09 +0200

    Add spinner for operator capability level diagram (#683)
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/src/layout/package/CapabilityLevel.tsx
+++ web/src/layout/package/CapabilityLevel.tsx
@@ -2,11 +2,10 @@ import classnames from 'classnames';
 import { isNull, isUndefined } from 'lodash';
 import React from 'react';
 import { FaCheck } from 'react-icons/fa';
-import { MdInfo } from 'react-icons/md';
 
-import Modal from '../common/Modal';
 import SmallTitle from '../common/SmallTitle';
 import styles from './CapabilityLevel.module.css';
+import CapatabilityLevelInfoModal from './CapatabilityLevelInfoModal';
 
 interface Props {
   capabilityLevel?: string;
@@ -23,20 +22,7 @@ const CapabilityLevel = (props: Props) => {
     <div>
       <div className="d-flex flex-row align-items-center">
         <SmallTitle text="Capability Level" />
-        <Modal
-          buttonType="btn-link btn-sm px-0 pb-0 text-secondary"
-          buttonContent={<MdInfo />}
-          className="d-none d-lg-inline-block mt-1 ml-2"
-          header={<div className="h5 m-0">Capability level</div>}
-        >
-          <div className="my-3 mw-100">
-            <img
-              src="/static/media/capability-level-diagram.svg"
-              alt="Capability Level Diagram"
-              className="capability-level-diagram mw-100"
-            />
-          </div>
-        </Modal>
+        <CapatabilityLevelInfoModal />
       </div>
 
       <div className="mb-3 position-relative">
