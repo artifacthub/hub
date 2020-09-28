f141cb388713ca0a258f1179230818eb2c6494f2
Cintia Sanchez Garcia <cynthiasg@icloud.com>
2020-09-28 12:59:36 +0200

    Add spinner for operator capability level diagram
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

---
+++ web/src/layout/package/CapatabilityLevelInfoModal.test.tsx
@@ -0,0 +1,29 @@
+import { fireEvent, render } from '@testing-library/react';
+import React from 'react';
+
+import CapatabilityLevelInfoModal from './CapatabilityLevelInfoModal';
+
+describe('CapatabilityLevelInfoModal', () => {
+  it('creates snapshot', () => {
+    const result = render(<CapatabilityLevelInfoModal />);
+    expect(result.asFragment()).toMatchSnapshot();
+  });
+
+  describe('Render', () => {
+    it('renders component', () => {
+      const { getByText } = render(<CapatabilityLevelInfoModal />);
+      expect(getByText('Capability level')).toBeInTheDocument();
+    });
+
+    it('opens modal', () => {
+      const { getByRole, getByTestId, getByAltText } = render(<CapatabilityLevelInfoModal />);
+
+      expect(getByRole('dialog')).not.toHaveClass('active');
+      const btn = getByTestId('openModalBtn');
+      fireEvent.click(btn);
+
+      expect(getByRole('dialog')).toHaveClass('active');
+      expect(getByAltText('Capability Level Diagram')).toBeInTheDocument();
+    });
+  });
+});
---
+++ web/src/layout/package/CapatabilityLevelInfoModal.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { MdInfo } from 'react-icons/md';
+
+import Loading from '../common/Loading';
+import Modal from '../common/Modal';
+import styles from './CapatabilityLevelInfoModal.module.css';
+
+const CapatabilityLevelInfoModal = () => {
+  const [onLoadedImage, setOnLoadedImage] = useState<boolean>(false);
+
+  return (
+    <Modal
+      className="d-none d-lg-inline-block mt-1 ml-2"
+      buttonContent={<MdInfo />}
+      buttonType="btn-link btn-sm px-0 pb-0 text-secondary"
+      header={<div className="h5 m-0">Capability level</div>}
+    >
+      <div className="my-3 mw-100">
+        <div className={styles.imageWrapper}>
+          {!onLoadedImage && <Loading />}
+          <img
+            src="/static/media/capability-level-diagram.svg"
+            alt="Capability Level Diagram"
+            className="capability-level-diagram mw-100"
+            onLoad={() => setOnLoadedImage(true)}
+          />
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default CapatabilityLevelInfoModal;
