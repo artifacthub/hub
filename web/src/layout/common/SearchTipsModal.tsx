7c0e75b558fc1871916b3d29cddc65e598829bb1
Cynthia S. Garcia <cynthiasg@icloud.com>
2020-10-05 08:50:50 +0200

    Fix issue closing search tips modal in UI (#708)
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/src/layout/common/SearchTipsModal.tsx
+++ web/src/layout/common/SearchTipsModal.tsx
@@ -17,59 +17,69 @@ const SearchTipsModal = (props: Props) => {
   const [openStatus, setOpenStatus] = useState<boolean>(false);
 
   return (
-    <Modal
+    <div
       className={classnames('position-absolute text-dark', styles.tipIcon, {
         [styles.bigTipIcon]: props.size === 'big',
       })}
-      buttonContent={<FaRegQuestionCircle />}
-      buttonType={classnames('btn-link px-0 pb-0 text-light', { 'btn-lg': props.size === 'big' })}
-      noFooter
-      onClose={() => setOpenStatus(false)}
-      open={openStatus}
     >
-      <div className="mw-100 text-left">
-        <div className="d-flex flex-row justify-content-between mb-4">
-          <div className={`h3 d-flex flex-row align-items-baseline ${styles.title}`}>
-            Search tips
-            <FaRegLightbulb className="ml-2" />
-          </div>
+      <button
+        data-testid="openSearchTipsBtn"
+        onClick={() => setOpenStatus(true)}
+        className={classnames('btn btn-link px-0 pb-0 text-light', {
+          'btn-lg': props.size === 'big',
+        })}
+      >
+        <FaRegQuestionCircle />
+      </button>
 
-          <div>
-            <button
-              data-testid="closeModalBtn"
-              type="button"
-              className={`close ${styles.closeModalBtn}`}
-              onClick={() => setOpenStatus(false)}
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-        </div>
+      <Modal noFooter onClose={() => setOpenStatus(false)} open={openStatus}>
+        <div className="mw-100 text-left">
+          <div className="d-flex flex-row justify-content-between mb-4">
+            <div className={`h3 d-flex flex-row align-items-baseline ${styles.title}`}>
+              Search tips
+              <FaRegLightbulb className="ml-2" />
+            </div>
 
-        <ul className="mb-0">
-          {SEARH_TIPS.map((tip: SearchTipItem, index: number) => (
-            <li data-testid="searchTip" className="my-1" key={`searchBarTip_${index}`}>
-              {tip.content} <small className="text-muted">Example:</small>{' '}
-              <Link
-                data-testid="searchTipLink"
-                className="font-weight-bold text-dark p-0"
-                onClick={() => setOpenStatus(false)}
-                to={{
-                  pathname: '/packages/search',
-                  search: prepareQueryString({
-                    pageNumber: 1,
-                    tsQueryWeb: tip.example,
-                    filters: {},
-                  }),
+            <div>
+              <button
+                data-testid="closeModalBtn"
+                type="button"
+                className={`close ${styles.closeModalBtn}`}
+                onClick={() => {
+                  console.log('click');
+                  setOpenStatus(false);
                 }}
               >
-                <u>{tip.example}</u>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </Modal>
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+          </div>
+
+          <ul className="mb-0">
+            {SEARH_TIPS.map((tip: SearchTipItem, index: number) => (
+              <li data-testid="searchTip" className="my-1" key={`searchBarTip_${index}`}>
+                {tip.content} <small className="text-muted">Example:</small>{' '}
+                <Link
+                  data-testid="searchTipLink"
+                  className="font-weight-bold text-dark p-0"
+                  onClick={() => setOpenStatus(false)}
+                  to={{
+                    pathname: '/packages/search',
+                    search: prepareQueryString({
+                      pageNumber: 1,
+                      tsQueryWeb: tip.example,
+                      filters: {},
+                    }),
+                  }}
+                >
+                  <u>{tip.example}</u>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Modal>
+    </div>
   );
 };
 
