bbc23b596021ba082c7d3e6adc5a89c4935683ee
Cynthia S. Garcia <cynthiasg@icloud.com>
2020-10-20 09:53:58 +0200

    Fix repo process time displayed in control panel (#763)
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/src/utils/minutesToNearestInterval.ts
+++ web/src/utils/minutesToNearestInterval.ts
@@ -4,6 +4,5 @@ export default (duration: number): number => {
   const rounding = moment.duration(duration, 'minutes');
   const currentDate = moment();
   const nextInterval = moment(Math.ceil(+currentDate / +rounding) * +rounding);
-  console.log(currentDate, nextInterval, currentDate.diff(nextInterval, 'minutes'));
-  return nextInterval.diff(currentDate, 'minutes');
+  return Math.ceil(nextInterval.diff(currentDate, 'minutes', true));
 };
