373c30b537da6d2c6a7a0e3b95826d744caf7328
Cintia Sanchez Garcia <cynthiasg@icloud.com>
2020-11-23 17:12:26 +0100

    Add missing frontend tests
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

---
+++ web/src/layout/controlPanel/settings/orgSettings/index.test.tsx
@@ -0,0 +1,49 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+import OrganizationSettingsSection from './index';
+
+jest.mock('../../../common/SectionPanel', () => (props: any) => <div>{props.defaultSection}</div>);
+
+const defaultProps = {
+  activeSection: 'user',
+  onAuthError: jest.fn(),
+};
+
+describe('OrganizationSettingsSection', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('renders correctly', () => {
+    const result = render(<OrganizationSettingsSection {...defaultProps} />);
+
+    expect(result.asFragment()).toMatchSnapshot();
+  });
+
+  describe('Render', () => {
+    it('renders profile section', () => {
+      const { getByText } = render(<OrganizationSettingsSection subsection="profile" {...defaultProps} />);
+
+      expect(getByText('profile')).toBeInTheDocument();
+    });
+
+    it('renders webhooks section', () => {
+      const { getByText } = render(<OrganizationSettingsSection subsection="webhooks" {...defaultProps} />);
+
+      expect(getByText('webhooks')).toBeInTheDocument();
+    });
+
+    it('renders authorization section', () => {
+      const { getByText } = render(<OrganizationSettingsSection subsection="authorization" {...defaultProps} />);
+
+      expect(getByText('authorization')).toBeInTheDocument();
+    });
+
+    it('renders profile as default section', () => {
+      const { getByText } = render(<OrganizationSettingsSection {...defaultProps} />);
+
+      expect(getByText('profile')).toBeInTheDocument();
+    });
+  });
+});
---
+++ web/src/layout/controlPanel/settings/userSettings/index.test.tsx
@@ -0,0 +1,55 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+import UserSettingsSection from './index';
+
+jest.mock('../../../common/SectionPanel', () => (props: any) => <div>{props.defaultSection}</div>);
+
+const defaultProps = {
+  activeSection: 'user',
+  onAuthError: jest.fn(),
+};
+
+describe('UserSettingsSection', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('renders correctly', () => {
+    const result = render(<UserSettingsSection {...defaultProps} />);
+
+    expect(result.asFragment()).toMatchSnapshot();
+  });
+
+  describe('Render', () => {
+    it('renders profile section', () => {
+      const { getByText } = render(<UserSettingsSection subsection="profile" {...defaultProps} />);
+
+      expect(getByText('profile')).toBeInTheDocument();
+    });
+
+    it('renders subscriptions section', () => {
+      const { getByText } = render(<UserSettingsSection subsection="subscriptions" {...defaultProps} />);
+
+      expect(getByText('subscriptions')).toBeInTheDocument();
+    });
+
+    it('renders apiKeys section', () => {
+      const { getByText } = render(<UserSettingsSection subsection="api-keys" {...defaultProps} />);
+
+      expect(getByText('api-keys')).toBeInTheDocument();
+    });
+
+    it('renders webhooks section', () => {
+      const { getByText } = render(<UserSettingsSection subsection="webhooks" {...defaultProps} />);
+
+      expect(getByText('webhooks')).toBeInTheDocument();
+    });
+
+    it('renders profile as default section', () => {
+      const { getByText } = render(<UserSettingsSection {...defaultProps} />);
+
+      expect(getByText('profile')).toBeInTheDocument();
+    });
+  });
+});
