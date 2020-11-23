15aeea595e4e08b54e5fa498e8e6560cef0a62ab
Cynthia S. Garcia <cynthiasg@icloud.com>
2020-11-23 19:44:22 +0100

    Use clipboard API in frontend (#889)
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/src/layout/common/ButtonCopyToClipboard.test.tsx
+++ web/src/layout/common/ButtonCopyToClipboard.test.tsx
@@ -1,16 +1,24 @@
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import React from 'react';
 
 import ButtonCopyToClipboard from './ButtonCopyToClipboard';
 
 const copyToClipboardMock = jest.fn();
+const clipboardWriteTextMock = jest.fn();
 
 document.execCommand = copyToClipboardMock;
+
+Object.defineProperty(navigator, 'clipboard', {
+  value: {
+    writeText: clipboardWriteTextMock,
+  },
+  writable: true,
+});
 
 jest.useFakeTimers();
 
 describe('ButtonCopyToClipboard', () => {
-  afterEach(() => {
+  beforeEach(() => {
     jest.resetAllMocks();
   });
 
@@ -19,26 +27,49 @@ describe('ButtonCopyToClipboard', () => {
     expect(asFragment).toMatchSnapshot();
   });
 
-  it('renders tooltip after clicking button', () => {
+  it('renders tooltip after clicking button', async () => {
     const { getByTestId, getByRole, queryByRole } = render(<ButtonCopyToClipboard text="Text to copy" />);
     expect(queryByRole('tooltip')).toBeNull();
 
     const btn = getByTestId('ctcBtn');
     fireEvent.click(btn);
-    expect(copyToClipboardMock).toHaveBeenCalledTimes(1);
-    expect(copyToClipboardMock).toHaveBeenCalledWith('copy');
+
+    await waitFor(() => {
+      expect(clipboardWriteTextMock).toHaveBeenCalledTimes(1);
+      expect(clipboardWriteTextMock).toHaveBeenCalledWith('Text to copy');
+    });
+
     expect(getByRole('tooltip')).toBeInTheDocument();
   });
 
-  it('hides tooltip after 2 seconds', () => {
+  it('hides tooltip after 2 seconds', async () => {
     const { getByTestId, getByRole, queryByRole } = render(<ButtonCopyToClipboard text="Text to copy" />);
     expect(queryByRole('tooltip')).toBeNull();
 
     const btn = getByTestId('ctcBtn');
     fireEvent.click(btn);
-    expect(getByRole('tooltip')).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(getByRole('tooltip')).toBeInTheDocument();
+    });
 
     expect(setTimeout).toHaveBeenCalledTimes(1);
     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 2000);
+  });
+
+  it('renders tooltip after clicking button when navidator.clipboard is undefined', async () => {
+    (navigator as any).clipboard = null;
+    const { getByTestId, getByRole, queryByRole } = render(<ButtonCopyToClipboard text="Text to copy" />);
+    expect(queryByRole('tooltip')).toBeNull();
+
+    const btn = getByTestId('ctcBtn');
+    fireEvent.click(btn);
+
+    await waitFor(() => {
+      expect(copyToClipboardMock).toHaveBeenCalledTimes(1);
+      expect(copyToClipboardMock).toHaveBeenCalledWith('copy');
+    });
+
+    expect(getByRole('tooltip')).toBeInTheDocument();
   });
 });
