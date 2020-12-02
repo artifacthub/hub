b0d10932faaef59078436afdebcb59e6fa16199b
Cintia Sanchez Garcia <cynthiasg@icloud.com>
2020-12-02 18:28:12 +0100

    Add blog link to homepage
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/src/layout/App.tsx
+++ web/src/layout/App.tsx
@@ -182,6 +182,9 @@ export default function App() {
                     <ExternalLink className="text-muted mb-1" href="https://artifacthub.github.io/hub/api/">
                       API docs
                     </ExternalLink>
+                    <ExternalLink className="text-muted mb-1" href="https://blog.artifacthub.io/blog/">
+                      Blog
+                    </ExternalLink>
                     <ExternalLink
                       className="text-muted mb-1"
                       href="https://github.com/cncf/foundation/blob/master/code-of-conduct.md"
