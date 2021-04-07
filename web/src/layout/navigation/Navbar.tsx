a03f6fede0b68dacddce5f39f613102754520374
Cintia Sanchez Garcia <cynthiasg@icloud.com>
2021-04-07 12:44:42 +0200

    Fix issue with signin/signup modals
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/src/layout/navigation/Navbar.tsx
+++ web/src/layout/navigation/Navbar.tsx
@@ -37,114 +37,120 @@ const Navbar = (props: Props) => {
   }, [props.redirect]); /* eslint-disable-line react-hooks/exhaustive-deps */
 
   return (
-    <nav
-      className={classnames('navbar navbar-top navbar-expand-md navbar-dark', styles.navbar, {
-        [styles.homeNavbar]: props.fromHome,
-      })}
-    >
-      <div className="container-lg px-sm-4 px-lg-0">
-        <div className={`d-flex flex-row ${styles.mobileWrapper}`}>
-          <Link data-testid="brandLink" className="navbar-brand d-flex align-items-center" to="/">
-            <FiHexagon className="mr-2" />
-            <div className="d-flex align-items-start">
-              <div className="d-flex align-items-baseline">
-                <span className="mr-1">Artifact</span>
-                <span className={styles.brand}>HUB</span>
+    <>
+      <nav
+        className={classnames('navbar navbar-top navbar-expand-md navbar-dark', styles.navbar, {
+          [styles.homeNavbar]: props.fromHome,
+        })}
+      >
+        <div className="container-lg px-sm-4 px-lg-0">
+          <div className={`d-flex flex-row ${styles.mobileWrapper}`}>
+            <Link data-testid="brandLink" className="navbar-brand d-flex align-items-center" to="/">
+              <FiHexagon className="mr-2" />
+              <div className="d-flex align-items-start">
+                <div className="d-flex align-items-baseline">
+                  <span className="mr-1">Artifact</span>
+                  <span className={styles.brand}>HUB</span>
+                </div>
+                <span
+                  className={`text-uppercase badge badge-pill badge-secondary d-flex align-items-center ${styles.badge}`}
+                >
+                  Beta
+                </span>
               </div>
-              <span
-                className={`text-uppercase badge badge-pill badge-secondary d-flex align-items-center ${styles.badge}`}
-              >
-                Beta
-              </span>
-            </div>
-          </Link>
-
-          <MobileSettings setOpenSignUp={setOpenSignUp} setOpenLogIn={setOpenLogIn} privateRoute={props.privateRoute} />
-        </div>
-
-        {openSignUp && <SignUp openSignUp={openSignUp} setOpenSignUp={setOpenSignUp} />}
-
-        {openLogIn && (
-          <LogIn
-            openLogIn={openLogIn}
-            setOpenLogIn={setOpenLogIn}
-            redirect={props.redirect}
-            visibleModal={props.visibleModal}
-          />
-        )}
-
-        {isUndefined(props.fromHome) && (
-          <SearchBar
-            size="normal"
-            formClassName={`mx-2 mr-md-auto my-3 my-md-0 flex-grow-1 pr-4 ${styles.search}`}
-            isSearching={props.isSearching}
-            tsQueryWeb={props.searchText}
-          />
-        )}
-
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav align-items-center ml-auto">
-            <Link
-              className={classnames(
-                'btn navbarBtn pl-0 pr-0 font-weight-bold text-uppercase position-relative text-nowrap',
-                styles.button
-              )}
-              to={{
-                pathname: '/stats',
-              }}
-            >
-              Stats
             </Link>
 
-            {isUndefined(ctx) || isUndefined(ctx.user) ? (
-              <div className="spinner-grow spinner-grow-sm textLight pt-1 ml-4" role="status">
-                <span className="sr-only">Loading...</span>
-              </div>
-            ) : (
-              <>
-                {isNull(ctx.user) ? (
-                  <>
-                    <li className="nav-item position-relative ml-4">
-                      <button
-                        type="button"
-                        className={classnames(
-                          'btn navbarBtn pl-0 pr-0 font-weight-bold text-uppercase position-relative text-nowrap',
-                          styles.button
-                        )}
-                        onClick={() => setOpenSignUp(true)}
-                      >
-                        Sign up
-                      </button>
-                    </li>
+            <MobileSettings
+              setOpenSignUp={setOpenSignUp}
+              setOpenLogIn={setOpenLogIn}
+              privateRoute={props.privateRoute}
+            />
+          </div>
 
-                    <li className="nav-item ml-4 position-relative">
-                      <button
-                        type="button"
-                        className={classnames(
-                          'btn navbarBtn font-weight-bold pr-0 pl-0 text-uppercase position-relative text-nowrap',
-                          styles.button
-                        )}
-                        onClick={() => setOpenLogIn(true)}
-                      >
-                        Sign in
-                      </button>
-                    </li>
+          {isUndefined(props.fromHome) && (
+            <SearchBar
+              size="normal"
+              formClassName={`mx-2 mr-md-auto my-3 my-md-0 flex-grow-1 pr-4 ${styles.search}`}
+              isSearching={props.isSearching}
+              tsQueryWeb={props.searchText}
+            />
+          )}
 
-                    <li className="nav-item ml-4 position-relative">
-                      <GuestDropdown />
-                    </li>
-                  </>
-                ) : (
-                  <li className="nav-item ml-4 position-relative">
-                    <UserAuthDropdown privateRoute={props.privateRoute} />
-                  </li>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav align-items-center ml-auto">
+              <Link
+                className={classnames(
+                  'btn navbarBtn pl-0 pr-0 font-weight-bold text-uppercase position-relative text-nowrap',
+                  styles.button
                 )}
-              </>
-            )}
-          </ul>
+                to={{
+                  pathname: '/stats',
+                }}
+              >
+                Stats
+              </Link>
+
+              {isUndefined(ctx) || isUndefined(ctx.user) ? (
+                <div className="spinner-grow spinner-grow-sm textLight pt-1 ml-4" role="status">
+                  <span className="sr-only">Loading...</span>
+                </div>
+              ) : (
+                <>
+                  {isNull(ctx.user) ? (
+                    <>
+                      <li className="nav-item position-relative ml-4">
+                        <button
+                          type="button"
+                          className={classnames(
+                            'btn navbarBtn pl-0 pr-0 font-weight-bold text-uppercase position-relative text-nowrap',
+                            styles.button
+                          )}
+                          onClick={() => setOpenSignUp(true)}
+                        >
+                          Sign up
+                        </button>
+                      </li>
+
+                      <li className="nav-item ml-4 position-relative">
+                        <button
+                          type="button"
+                          className={classnames(
+                            'btn navbarBtn font-weight-bold pr-0 pl-0 text-uppercase position-relative text-nowrap',
+                            styles.button
+                          )}
+                          onClick={() => setOpenLogIn(true)}
+                        >
+                          Sign in
+                        </button>
+                      </li>
+
+                      <li className="nav-item ml-4 position-relative">
+                        <GuestDropdown />
+                      </li>
+                    </>
+                  ) : (
+                    <li className="nav-item ml-4 position-relative">
+                      <UserAuthDropdown privateRoute={props.privateRoute} />
+                    </li>
+                  )}
+                </>
+              )}
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      {openSignUp && <SignUp openSignUp={openSignUp} setOpenSignUp={setOpenSignUp} />}
+
+      {openLogIn && (
+        <LogIn
+          openLogIn={openLogIn}
+          setOpenLogIn={setOpenLogIn}
+          redirect={props.redirect}
+          visibleModal={props.visibleModal}
+        />
+      )}
+    </>
   );
 };
 
