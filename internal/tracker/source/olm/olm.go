cf554ebcdbc7c2ed8d415beb80411a049d658944
Sergio C. Arteaga <tegioz@icloud.com>
2021-05-12 14:17:59 +0200

    Add support for OLM operators bundle format (#1309)
    Closes #1306
    
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- internal/tracker/source/olm/olm.go
+++ internal/tracker/source/olm/olm.go
@@ -23,6 +23,10 @@ import (
 )
 
 const (
+	bundle          = "bundle"
+	packageManifest = "packageManifest"
+
+	// Artifact Hub special annotations
 	changesAnnotation         = "artifacthub.io/changes"
 	imagesWhitelistAnnotation = "artifacthub.io/imagesWhitelist"
 	installAnnotation         = "artifacthub.io/install"
@@ -34,7 +38,7 @@ const (
 
 var (
 	// channelVersionRE is a regexp used to extract the version from the
-	// channel CurrentCSVName.
+	// channel CurrentCSVName in the PackageManifest format.
 	channelVersionRE = regexp.MustCompile(`^[A-Za-z0-9_-]+\.v?(.*)$`)
 )
 
@@ -53,7 +57,7 @@ func (s *TrackerSource) GetPackagesAvailable() (map[string]*hub.Package, error) 
 	packagesAvailable := make(map[string]*hub.Package)
 
 	// Walk the path provided looking for available packages
-	err := filepath.Walk(s.i.BasePath, func(pkgPath string, info os.FileInfo, err error) error {
+	err := filepath.Walk(s.i.BasePath, func(path string, info os.FileInfo, err error) error {
 		// Return ASAP if context is cancelled
 		select {
 		case <-s.i.Svc.Ctx.Done():
@@ -67,61 +71,30 @@ func (s *TrackerSource) GetPackagesAvailable() (map[string]*hub.Package, error) 
 			return nil
 		}
 
-		// Get package manifest
-		manifest, err := getManifest(pkgPath)
+		// Get package version metadata
+		md, err := getMetadata(path)
 		if err != nil {
-			s.warn(fmt.Errorf("error getting package manifest: %w", err))
+			s.warn(fmt.Errorf("error getting package metadata: %w", err))
 			return nil
 		}
-		if manifest == nil {
-			// Package manifest not found, not a package path
+		if md == nil {
+			// Not a package path
 			return nil
 		}
 
-		// Get package versions
-		pkgName := manifest.PackageName
-		versionsUnfiltered, err := ioutil.ReadDir(pkgPath)
+		// Validate version
+		if _, err := semver.StrictNewVersion(md.Version); err != nil {
+			s.warn(fmt.Errorf("invalid package %s version (%s): %w", md.Name, md.Version, err))
+			return nil
+		}
+
+		// Prepare and store package version
+		p, err := s.preparePackage(s.i.Repository, md)
 		if err != nil {
-			s.warn(fmt.Errorf("error reading package %s versions: %w", pkgName, err))
+			s.warn(fmt.Errorf("error preparing package %s version %s: %w", md.Name, md.Version, err))
 			return nil
 		}
-		var versions []os.FileInfo
-		for _, entryV := range versionsUnfiltered {
-			if !entryV.IsDir() {
-				continue
-			}
-			if _, err := semver.StrictNewVersion(entryV.Name()); err != nil {
-				s.warn(fmt.Errorf("invalid package %s version (%s): %w", pkgName, entryV.Name(), err))
-				continue
-			} else {
-				versions = append(versions, entryV)
-			}
-		}
-		sort.Slice(versions, func(i, j int) bool {
-			vi, _ := semver.NewVersion(versions[i].Name())
-			vj, _ := semver.NewVersion(versions[j].Name())
-			return vj.LessThan(vi)
-		})
-
-		// Process package versions
-		for _, entryV := range versions {
-			// Get package version CSV
-			version := entryV.Name()
-			pkgVersionPath := filepath.Join(pkgPath, version)
-			csv, csvData, err := getCSV(pkgVersionPath)
-			if err != nil {
-				s.warn(fmt.Errorf("error getting package %s version %s csv: %w", pkgName, version, err))
-				continue
-			}
-
-			// Prepare and store package version
-			p, err := s.preparePackage(s.i.Repository, manifest, csv, csvData)
-			if err != nil {
-				s.warn(fmt.Errorf("error preparing package %s version %s: %w", pkgName, version, err))
-				continue
-			}
-			packagesAvailable[pkg.BuildKey(p)] = p
-		}
+		packagesAvailable[pkg.BuildKey(p)] = p
 
 		return nil
 	})
@@ -129,54 +102,52 @@ func (s *TrackerSource) GetPackagesAvailable() (map[string]*hub.Package, error) 
 		return nil, err
 	}
 
+	preparePackagesChannels(packagesAvailable)
+
 	return packagesAvailable, nil
 }
 
-// preparePackage prepares a package version using the package manifest and csv.
-func (s *TrackerSource) preparePackage(
-	r *hub.Repository,
-	manifest *manifests.PackageManifest,
-	csv *operatorsv1alpha1.ClusterServiceVersion,
-	csvData []byte,
-) (*hub.Package, error) {
+// preparePackage prepares a package version using the provided metadata.
+func (s *TrackerSource) preparePackage(r *hub.Repository, md *Metadata) (*hub.Package, error) {
 	// Prepare package from manifest and csv
 	p := &hub.Package{
-		Name:           manifest.PackageName,
-		DisplayName:    csv.Spec.DisplayName,
-		Description:    csv.Annotations["description"],
-		Keywords:       csv.Spec.Keywords,
-		Readme:         csv.Spec.Description,
-		Version:        csv.Spec.Version.String(),
+		Name:           md.Name,
+		DisplayName:    md.CSV.Spec.DisplayName,
+		Description:    md.CSV.Annotations["description"],
+		Keywords:       md.CSV.Spec.Keywords,
+		Readme:         md.CSV.Spec.Description,
+		Version:        md.CSV.Spec.Version.String(),
 		IsOperator:     true,
-		Capabilities:   csv.Annotations["capabilities"],
-		DefaultChannel: manifest.DefaultChannelName,
-		License:        csv.Annotations[licenseAnnotation],
-		Provider:       csv.Spec.Provider.Name,
-		Install:        csv.Annotations[installAnnotation],
+		Capabilities:   md.CSV.Annotations["capabilities"],
+		Channels:       md.Channels,
+		DefaultChannel: md.DefaultChannelName,
+		License:        md.CSV.Annotations[licenseAnnotation],
+		Provider:       md.CSV.Spec.Provider.Name,
+		Install:        md.CSV.Annotations[installAnnotation],
 		Repository:     r,
 	}
 
 	// Containers images
-	containersImages, err := getContainersImages(csv, csvData)
+	containersImages, err := getContainersImages(md.CSV, md.CSVData)
 	if err != nil {
 		return nil, err
 	}
 	p.ContainersImages = containersImages
 
 	// TS
-	ts, err := time.Parse(time.RFC3339, csv.Annotations["createdAt"])
+	ts, err := time.Parse(time.RFC3339, md.CSV.Annotations["createdAt"])
 	if err == nil {
 		p.TS = ts.Unix()
 	} else {
 		// Try alternative layout
-		ts, err = time.Parse("2006-01-02 15:04:05", csv.Annotations["createdAt"])
+		ts, err = time.Parse("2006-01-02 15:04:05", md.CSV.Annotations["createdAt"])
 		if err == nil {
 			p.TS = ts.Unix()
 		}
 	}
 
 	// Keywords
-	for _, category := range strings.Split(csv.Annotations["categories"], ",") {
+	for _, category := range strings.Split(md.CSV.Annotations["categories"], ",") {
 		if strings.Trim(strings.ToLower(category), " ") == "ai/machine learning" {
 			p.Keywords = append(p.Keywords, []string{"AI", "Machine Learning"}...)
 		} else {
@@ -185,22 +156,22 @@ func (s *TrackerSource) preparePackage(
 	}
 
 	// Links
-	for _, link := range csv.Spec.Links {
+	for _, link := range md.CSV.Spec.Links {
 		p.Links = append(p.Links, &hub.Link{
 			Name: link.Name,
 			URL:  link.URL,
 		})
 	}
-	if csv.Annotations["repository"] != "" {
+	if md.CSV.Annotations["repository"] != "" {
 		p.Links = append(p.Links, &hub.Link{
 			Name: "source",
-			URL:  csv.Annotations["repository"],
+			URL:  md.CSV.Annotations["repository"],
 		})
 	}
 
 	// Store logo when available
-	if len(csv.Spec.Icon) > 0 && csv.Spec.Icon[0].Data != "" {
-		data, err := base64.StdEncoding.DecodeString(csv.Spec.Icon[0].Data)
+	if len(md.CSV.Spec.Icon) > 0 && md.CSV.Spec.Icon[0].Data != "" {
+		data, err := base64.StdEncoding.DecodeString(md.CSV.Spec.Icon[0].Data)
 		if err != nil {
 			s.warn(fmt.Errorf("error decoding package %s logo image: %w", p.Name, err))
 		} else {
@@ -212,29 +183,16 @@ func (s *TrackerSource) preparePackage(
 	}
 
 	// Maintainers
-	for _, maintainer := range csv.Spec.Maintainers {
+	for _, maintainer := range md.CSV.Spec.Maintainers {
 		p.Maintainers = append(p.Maintainers, &hub.Maintainer{
 			Name:  maintainer.Name,
 			Email: maintainer.Email,
 		})
 	}
 
-	// Channels
-	for _, channel := range manifest.Channels {
-		matches := channelVersionRE.FindStringSubmatch(channel.CurrentCSVName)
-		if len(matches) != 2 {
-			continue
-		}
-		version := matches[1]
-		p.Channels = append(p.Channels, &hub.Channel{
-			Name:    channel.Name,
-			Version: version,
-		})
-	}
-
 	// CRDs
-	crds := make([]interface{}, 0, len(csv.Spec.CustomResourceDefinitions.Owned))
-	for _, crd := range csv.Spec.CustomResourceDefinitions.Owned {
+	crds := make([]interface{}, 0, len(md.CSV.Spec.CustomResourceDefinitions.Owned))
+	for _, crd := range md.CSV.Spec.CustomResourceDefinitions.Owned {
 		crds = append(crds, map[string]interface{}{
 			"name":        crd.Name,
 			"version":     crd.Version,
@@ -247,12 +205,12 @@ func (s *TrackerSource) preparePackage(
 		p.CRDs = crds
 	}
 	var crdsExamples []interface{}
-	if err := json.Unmarshal([]byte(csv.Annotations["alm-examples"]), &crdsExamples); err == nil {
+	if err := json.Unmarshal([]byte(md.CSV.Annotations["alm-examples"]), &crdsExamples); err == nil {
 		p.CRDsExamples = crdsExamples
 	}
 
 	// Changes
-	if v, ok := csv.Annotations[changesAnnotation]; ok {
+	if v, ok := md.CSV.Annotations[changesAnnotation]; ok {
 		changes, err := source.ParseChangesAnnotation(v)
 		if err != nil {
 			return nil, err
@@ -261,7 +219,7 @@ func (s *TrackerSource) preparePackage(
 	}
 
 	// Prerelease
-	if v, ok := csv.Annotations[prereleaseAnnotation]; ok {
+	if v, ok := md.CSV.Annotations[prereleaseAnnotation]; ok {
 		prerelease, err := strconv.ParseBool(v)
 		if err != nil {
 			return nil, fmt.Errorf("invalid prerelease value: %s", v)
@@ -270,7 +228,7 @@ func (s *TrackerSource) preparePackage(
 	}
 
 	// Recommendations
-	if v, ok := csv.Annotations[recommendationsAnnotation]; ok {
+	if v, ok := md.CSV.Annotations[recommendationsAnnotation]; ok {
 		var recommendations []*hub.Recommendation
 		if err := yaml.Unmarshal([]byte(v), &recommendations); err != nil {
 			return nil, fmt.Errorf("invalid recommendations value: %s", v)
@@ -279,7 +237,7 @@ func (s *TrackerSource) preparePackage(
 	}
 
 	// Security updates
-	if v, ok := csv.Annotations[securityUpdatesAnnotation]; ok {
+	if v, ok := md.CSV.Annotations[securityUpdatesAnnotation]; ok {
 		containsSecurityUpdates, err := strconv.ParseBool(v)
 		if err != nil {
 			return nil, fmt.Errorf("invalid containsSecurityUpdates value: %s", v)
@@ -289,12 +247,13 @@ func (s *TrackerSource) preparePackage(
 
 	// Prepare data specific to the package kind
 	var isGlobalOperator bool
-	for _, e := range csv.Spec.InstallModes {
+	for _, e := range md.CSV.Spec.InstallModes {
 		if e.Type == operatorsv1alpha1.InstallModeTypeAllNamespaces && e.Supported {
 			isGlobalOperator = true
 		}
 	}
 	p.Data = map[string]interface{}{
+		"format":           md.Format,
 		"isGlobalOperator": isGlobalOperator,
 	}
 
@@ -308,10 +267,91 @@ func (s *TrackerSource) warn(err error) {
 	s.i.Svc.Ec.Append(s.i.Repository.RepositoryID, err.Error())
 }
 
-// getManifest reads and parses the package manifest.
-func getManifest(pkgPath string) (*manifests.PackageManifest, error) {
+// Metadata represents some information about an OLM operator version.
+type Metadata struct {
+	Format             string
+	Name               string
+	Version            string
+	Channels           []*hub.Channel
+	DefaultChannelName string
+	CSV                *operatorsv1alpha1.ClusterServiceVersion
+	CSVData            []byte
+	CSVPath            string
+}
+
+// getMetadata returns the metadata for the package version located in the path
+// provided.
+func getMetadata(path string) (*Metadata, error) {
+	var md *Metadata
+
+	// PackageManifest format
+	manifest, err := getManifest(filepath.Join(path, ".."))
+	if err != nil {
+		return nil, err
+	}
+	if manifest != nil {
+		csv, csvData, err := getCSV(path)
+		if err != nil {
+			return nil, fmt.Errorf("error getting package %s csv (path: %s): %w", manifest.PackageName, path, err)
+		}
+		var channels []*hub.Channel
+		for _, channel := range manifest.Channels {
+			matches := channelVersionRE.FindStringSubmatch(channel.CurrentCSVName)
+			if len(matches) != 2 {
+				continue
+			}
+			version := matches[1]
+			channels = append(channels, &hub.Channel{
+				Name:    channel.Name,
+				Version: version,
+			})
+		}
+		md = &Metadata{
+			Format:             packageManifest,
+			Name:               manifest.PackageName,
+			Version:            csv.Spec.Version.String(),
+			Channels:           channels,
+			DefaultChannelName: manifest.DefaultChannelName,
+			CSV:                csv,
+			CSVData:            csvData,
+		}
+	}
+
+	// Bundle format
+	annotations, err := getBundleAnnotations(filepath.Join(path, "metadata"))
+	if err != nil {
+		return nil, err
+	}
+	if annotations != nil {
+		csv, csvData, err := getCSV(filepath.Join(path, "manifests"))
+		if err != nil {
+			return nil, fmt.Errorf("error getting package %s csv (path: %s): %w", annotations.PackageName, path, err)
+		}
+		var channels []*hub.Channel
+		for _, channelName := range strings.Split(annotations.Channels, ",") {
+			channels = append(channels, &hub.Channel{
+				Name:    channelName,
+				Version: csv.Spec.Version.String(),
+			})
+		}
+		md = &Metadata{
+			Format:             bundle,
+			Name:               annotations.PackageName,
+			Version:            csv.Spec.Version.String(),
+			Channels:           channels, // Will be updated when all package versions are processed
+			DefaultChannelName: annotations.DefaultChannelName,
+			CSV:                csv,
+			CSVData:            csvData,
+		}
+	}
+
+	return md, nil
+}
+
+// getManifest reads and parses the package's manifest.
+func getManifest(path string) (*manifests.PackageManifest, error) {
 	// Locate manifest file
-	matches, err := filepath.Glob(filepath.Join(pkgPath, "*package.yaml"))
+	matches, err := filepath.Glob(filepath.Join(path, "*package.yaml"))
 	if err != nil {
 		return nil, fmt.Errorf("error locating manifest file: %w", err)
 	}
@@ -333,8 +373,29 @@ func getManifest(pkgPath string) (*manifests.PackageManifest, error) {
 	return manifest, nil
 }
 
-// getCSV reads, parses and validates the cluster service version file of the
-// given package version.
+// getBundleAnnotations reads and parses the bundle's annotations.
+func getBundleAnnotations(path string) (*manifests.Annotations, error) {
+	// Check if annotations file exists
+	annotationsPath := filepath.Join(path, "annotations.yaml")
+	if _, err := os.Stat(annotationsPath); os.IsNotExist(err) {
+		return nil, nil
+	}
+
+	// Read and parse annotations file
+	annotationsData, err := ioutil.ReadFile(annotationsPath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading annotations file: %w", err)
+	}
+	annotationsFile := &manifests.AnnotationsFile{}
+	if err = yaml.Unmarshal(annotationsData, &annotationsFile); err != nil {
+		return nil, fmt.Errorf("error unmarshaling annotations file: %w", err)
+	}
+
+	return &annotationsFile.Annotations, nil
+}
+
+// getCSV reads and parses the cluster service version file in the path
+// provided, when available.
 func getCSV(path string) (*operatorsv1alpha1.ClusterServiceVersion, []byte, error) {
 	// Locate cluster service version file
 	matches, err := filepath.Glob(filepath.Join(path, "*.clusterserviceversion.yaml"))
@@ -398,6 +459,69 @@ func getContainersImages(
 	}
 
 	return images, nil
+}
+
+// preparePackagesChannels prepares and updates the channels in the packages
+// using the bundle format.
+//
+// At the moment, when registering an OLM operator package in Artifact Hub, the
+// the latest version of the package is expected to provide all channels
+// available with the corresponding version they point to. When using the
+// PackageManifest format, this information is read from the package manifest
+// file. However, when using the Bundle format, this information is not
+// available on a single location, so it needs to be prepared from the data
+// available in all packages' versions.
+func preparePackagesChannels(packages map[string]*hub.Package) {
+	usesBundleFormat := func(p *hub.Package) bool {
+		format, ok := p.Data["format"].(string)
+		return ok && format == bundle
+	}
+
+	// Pass 1: track latest packages' channels version
+	channels := make(map[string]map[string]string) // packageName:channelName:channelVersion
+	for _, p := range packages {
+		if !usesBundleFormat(p) {
+			continue
+		}
+		for _, c := range p.Channels {
+			if _, ok := channels[p.Name]; !ok {
+				channels[p.Name] = map[string]string{c.Name: c.Version}
+			} else {
+				latestVersion, ok := channels[p.Name][c.Name]
+				if !ok {
+					channels[p.Name][c.Name] = c.Version
+				} else {
+					versionSV, _ := semver.StrictNewVersion(c.Version)
+					latestVersionSV, _ := semver.StrictNewVersion(latestVersion)
+					if versionSV.GreaterThan(latestVersionSV) {
+						channels[p.Name][c.Name] = c.Version
+					}
+				}
+			}
+		}
+	}
+
+	// Prepare packages channels
+	preparedChannels := make(map[string][]*hub.Channel)
+	for pkgName, pkgChannels := range channels {
+		for name, version := range pkgChannels {
+			preparedChannels[pkgName] = append(preparedChannels[pkgName], &hub.Channel{
+				Name:    name,
+				Version: version,
+			})
+		}
+		sort.Slice(preparedChannels[pkgName], func(i, j int) bool {
+			return preparedChannels[pkgName][i].Name < preparedChannels[pkgName][j].Name
+		})
+	}
+
+	// Pass 2: update the corresponding packages' channels
+	for _, p := range packages {
+		if !usesBundleFormat(p) {
+			continue
+		}
+		p.Channels = preparedChannels[p.Name]
+	}
 }
 
 // contains is a helper to check if a list contains the string provided.
