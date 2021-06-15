3aba3a9549934ec4269efccd88ad39e1aff3b85a
Sergio C. Arteaga <tegioz@icloud.com>
2021-06-15 10:51:14 +0200

    Update repositories search max limit (#1394)
    The new max limit is 60.
    
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- internal/handlers/repo/handlers.go
+++ internal/handlers/repo/handlers.go
@@ -19,7 +19,7 @@ import (
 const (
 	logoSVG            = `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-hexagon"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path></svg>`
 	searchDefaultLimit = 20
-	searchMaxLimit     = 50
+	searchMaxLimit     = 60
 )
 
 // Handlers represents a group of http handlers in charge of handling
