237d5df57d48819e8c506da01b9a09a83b02c253
Cintia Sanchez Garcia <cynthiasg@icloud.com>
2021-11-15 10:58:45 +0100

    Proxy API requests in development environment
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/src/utils/getHubBaseURL.ts
+++
@@ -1,8 +0,0 @@
-export default (): string => {
-  let hubBaseUrl = '';
-  if (process.env.NODE_ENV === 'development') {
-    hubBaseUrl = `${process.env.REACT_APP_HUB_BASE_URL}`;
-  }
-
-  return hubBaseUrl;
-};
