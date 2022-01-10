41e8d1f83156bca01c95a5bcfda686160821b217
Sergio Castaño Arteaga <tegioz@icloud.com>
2022-01-10 11:02:32 +0100

    Fix broken test
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- database/migrations/functions/packages/get_package_views.sql
+++ database/migrations/functions/packages/get_package_views.sql
@@ -1,12 +1,14 @@
--- get_package_views returns the number of views per day for the given package
--- organized by version as a json object.
-create or replace function get_package_views(p_package_id uuid)
+-- get_package_views returns the number of views per day in the time range
+-- delimited by the start and end provided for the given package organized by
+-- version as a json object.
+create or replace function get_package_views(p_package_id uuid, p_start date, p_end date)
 returns setof json as $$
     with last_month_views as (
         select version, day, total
         from package_views
         where package_id = p_package_id
-        and day >= current_date - '1 month'::interval
+        and day >= p_start
+        and day <= p_end
     )
     select coalesce(json_object_agg(version, (
         select json_object_agg(day, total)
