cad5608fbcdba8bddbc5e69423313724d5d03884
Cintia Sanchez Garcia <cynthiasg@icloud.com>
2022-04-21 13:33:41 +0200

    Add support for comparing Helm charts templates versions
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

---
+++ web/src/layout/package/chartTemplates/CompareView.tsx
@@ -0,0 +1,175 @@
+import { differenceBy, isNull, isUndefined, sortBy } from 'lodash';
+import { useEffect, useState } from 'react';
+import { BsArrowsCollapse, BsArrowsExpand } from 'react-icons/bs';
+
+import API from '../../../api';
+import { ChartTemplate, CompareChartTemplate, CompareChartTemplateStatus, TemplatesQuery } from '../../../types';
+import ErrorBoundary from '../../common/ErrorBoundary';
+import Loading from '../../common/Loading';
+import CompareTemplatesList from './CompareTemplatesList';
+import styles from './CompareView.module.css';
+import DiffTemplate from './DiffTemplate';
+
+interface Props {
+  packageId: string;
+  templates: ChartTemplate[] | null;
+  currentVersion: string;
+  visibleTemplate?: string;
+  comparedVersion: string;
+  formatTemplates: (templates: ChartTemplate[]) => ChartTemplate[];
+  updateUrl: (q: TemplatesQuery) => void;
+}
+
+const CompareView = (props: Props) => {
+  const [diffTemplates, setDiffTemplates] = useState<ChartTemplate[] | null | undefined>();
+  const [activeTemplate, setActiveTemplate] = useState<CompareChartTemplate | null>(null);
+  const [isChangingTemplate, setIsChangingTemplate] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [visibleTemplates, setVisibleTemplates] = useState<CompareChartTemplate[] | undefined>();
+  const [expanded, setExpanded] = useState<boolean>(false);
+
+  const onTemplateChange = (template: CompareChartTemplate | null) => {
+    setIsChangingTemplate(true);
+    setExpanded(false);
+    setActiveTemplate(template);
+    props.updateUrl({ template: template ? template.name : undefined, compareTo: props.comparedVersion });
+  };
+
+  useEffect(() => {
+    async function getDiffCompareChartTemplates(version: string) {
+      try {
+        setIsChangingTemplate(true);
+        setIsLoading(true);
+        const data = await API.getChartTemplates(props.packageId, version);
+        if (data && data.templates) {
+          const formattedTemplates: ChartTemplate[] = props.formatTemplates(data.templates);
+          if (formattedTemplates.length > 0) {
+            setDiffTemplates(formattedTemplates);
+          } else {
+            setDiffTemplates([]);
+          }
+        }
+      } catch {
+        setDiffTemplates(null);
+        setIsChangingTemplate(false);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    if (props.comparedVersion !== '') {
+      getDiffCompareChartTemplates(props.comparedVersion);
+    } else {
+      setDiffTemplates([]);
+    }
+  }, [props.comparedVersion]); /* eslint-disable-line react-hooks/exhaustive-deps */
+
+  useEffect(() => {
+    const prepareVisibleTemplates = () => {
+      let tmpls: CompareChartTemplate[] = [];
+      props.templates!.forEach((tmpl: ChartTemplate) => {
+        const diffTmpl = diffTemplates!.find((t: ChartTemplate) => t.name === tmpl.name);
+        if (diffTmpl) {
+          if (diffTmpl.data !== tmpl.data) {
+            tmpls.push({ ...tmpl, compareData: diffTmpl.data, status: CompareChartTemplateStatus.Modified });
+          }
+        } else {
+          tmpls.push({
+            ...tmpl,
+            data: '',
+            compareData: tmpl.data,
+            status: CompareChartTemplateStatus.Added,
+          });
+        }
+      });
+      const others = differenceBy(diffTemplates!, props.templates!, 'name');
+      others.forEach((tmpl: ChartTemplate) => {
+        tmpls.push({
+          ...tmpl,
+          data: tmpl.data,
+          compareData: '',
+          status: CompareChartTemplateStatus.Deleted,
+        });
+      });
+      const sortedTmpls: CompareChartTemplate[] = sortBy(tmpls, ['type', 'name']);
+      setVisibleTemplates(sortedTmpls);
+      if (sortedTmpls.length === 0) {
+        setActiveTemplate(null);
+      } else {
+        if (props.visibleTemplate) {
+          const selectedTmpl = sortedTmpls.find((tmpl: CompareChartTemplate) => props.visibleTemplate === tmpl.name);
+          if (selectedTmpl) {
+            setActiveTemplate(selectedTmpl);
+          } else {
+            setActiveTemplate(sortedTmpls[0]);
+          }
+        } else {
+          setActiveTemplate(sortedTmpls[0]);
+        }
+      }
+    };
+
+    if (diffTemplates && !isNull(props.templates)) {
+      prepareVisibleTemplates();
+      setIsChangingTemplate(false);
+    }
+  }, [diffTemplates]); /* eslint-disable-line react-hooks/exhaustive-deps */
+
+  return (
+    <div className="d-flex flex-row align-items-stretch g-0 h-100 mh-100">
+      <div className="col-3 h-100">
+        <CompareTemplatesList
+          templates={visibleTemplates}
+          activeTemplateName={activeTemplate ? activeTemplate.name : undefined}
+          onTemplateChange={onTemplateChange}
+        />
+      </div>
+
+      <div className="col-9 ps-3 h-100">
+        <div className={`position-relative h-100 mh-100 border ${styles.templateWrapper}`}>
+          {((isChangingTemplate && activeTemplate) || isLoading) && <Loading />}
+          <div className={`position-absolute d-flex ${styles.wrapper}`}>
+            <div className="position-relative">
+              <button
+                className={`btn btn-sm btn-primary rounded-circle fs-5 ${styles.btn}`}
+                onClick={() => {
+                  setExpanded(!expanded);
+                }}
+                aria-label={`${expanded ? 'Collapse' : 'Expand'} code`}
+                disabled={!isUndefined(visibleTemplates) && visibleTemplates.length === 0}
+              >
+                {expanded ? <BsArrowsCollapse /> : <BsArrowsExpand />}
+              </button>
+            </div>
+          </div>
+
+          <pre className={`text-muted h-100 mh-100 mb-0 overflow-hidden position-relative diffTemplate ${styles.pre}`}>
+            {!isUndefined(visibleTemplates) && visibleTemplates.length === 0 && (
+              <div className="d-flex align-items-center justify-content-center h-100 w-100 p-5">
+                <div className={`alert alert-dark px-5 py-4 text-center ${styles.alert}`}>
+                  <span className="text-muted">
+                    No changes found when comparing version <span className="fw-bold">{props.currentVersion}</span> to{' '}
+                    <span className="fw-bold">{props.comparedVersion}</span>
+                  </span>
+                </div>
+              </div>
+            )}
+            {activeTemplate && (
+              <ErrorBoundary className={styles.errorAlert} message="Something went wrong rendering the template.">
+                <DiffTemplate
+                  currentVersion={props.currentVersion}
+                  diffVersion={props.comparedVersion}
+                  template={activeTemplate!}
+                  expanded={expanded}
+                  setIsChangingTemplate={setIsChangingTemplate}
+                />
+              </ErrorBoundary>
+            )}
+          </pre>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CompareView;
---
+++ web/src/layout/package/chartTemplates/DiffTemplate.tsx
@@ -0,0 +1,150 @@
+import 'react-diff-view/style/index.css';
+
+import classnames from 'classnames';
+import { isNull } from 'lodash';
+import { Dispatch, Fragment, SetStateAction, useCallback, useEffect, useRef, useState } from 'react';
+import { Decoration, Diff, Hunk, parseDiff } from 'react-diff-view';
+
+import { CompareChartTemplate, CompareChartTemplateStatus } from '../../../types';
+import styles from './DiffTemplate.module.css';
+
+const DiffLibrary = require('diff');
+
+interface Props {
+  currentVersion: string;
+  diffVersion: string;
+  template: CompareChartTemplate;
+  expanded: boolean;
+  setIsChangingTemplate: Dispatch<SetStateAction<boolean>>;
+}
+
+interface DiffProps {
+  diffText: string;
+  fileName: string;
+  status?: CompareChartTemplateStatus;
+  removeLoading: () => void;
+}
+
+const Changes = (props: DiffProps) => {
+  const tmplWrapper = useRef<HTMLDivElement>(null);
+  const files = parseDiff(props.diffText);
+
+  const scrollTop = () => {
+    if (tmplWrapper && tmplWrapper.current) {
+      tmplWrapper.current.scroll(0, 0);
+    }
+  };
+
+  const renderFile = ({
+    oldPath,
+    newPath,
+    oldRevision,
+    newRevision,
+    type,
+    hunks,
+    oldEndingNewLine,
+    newEndingNewLine,
+  }: any) => {
+    return (
+      <div key={`${oldRevision}-${newRevision}`} className="file-diff h-100">
+        <header
+          className={`d-flex flex-row align-items-center justify-content-between diff-header fw-bold py-1 ${styles.header}`}
+        >
+          <div className="text-truncate">{props.fileName}</div>
+          <div className="pe-5 me-3 ps-3">
+            <span className={`pe-2 ${styles.versionTitle}`}>Changes from</span>
+            <span className="badge bg-dark px-2 py-1 badge-md">{oldPath}</span>
+            <span className={`px-2 ${styles.versionTitle}`}>to</span>
+            <span className="badge bg-dark px-2 py-1 badge-md">{newPath}</span>
+          </div>
+        </header>
+        <div ref={tmplWrapper} className={`overflow-scroll border-top py-2 ${styles.codeWrapper}`}>
+          <Diff viewType="unified" diffType={type} hunks={hunks || []}>
+            {(hunks: any[]) => (
+              <>
+                {hunks.map((hunk, index) => {
+                  return [
+                    <Decoration key={`deco-${hunk.content}-${index}`}>
+                      <div
+                        className={classnames(
+                          'hunk-header py-1 px-3 fw-bold',
+                          styles.hunkHeader,
+                          { 'mb-2': index === 0 },
+                          { 'my-2': index !== 0 }
+                        )}
+                      >
+                        {hunk.content}
+                      </div>
+                    </Decoration>,
+                    <Hunk key={`${hunk.content}-${index}`} hunk={hunk} />,
+                    <Fragment key={`${hunk.content}-newLine-${index}`}>
+                      {index + 1 === hunks.length &&
+                        !oldEndingNewLine &&
+                        props.status !== CompareChartTemplateStatus.Deleted && (
+                          <tbody>
+                            <tr>
+                              <td></td>
+                              <td></td>
+                              <td className={styles.newLine}>\ No newline at end of file</td>
+                            </tr>
+                          </tbody>
+                        )}
+                    </Fragment>,
+                  ];
+                })}
+              </>
+            )}
+          </Diff>
+        </div>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    props.removeLoading();
+    scrollTop();
+  }, [props.diffText]); /* eslint-disable-line react-hooks/exhaustive-deps */
+
+  return <>{files.map(renderFile)}</>;
+};
+
+const DiffTemplate = (props: Props) => {
+  const [diffContent, setDiffContent] = useState<string | null>(null);
+
+  const removeLoading = useCallback(() => {
+    props.setIsChangingTemplate(false);
+  }, []); /* eslint-disable-line react-hooks/exhaustive-deps */
+
+  useEffect(() => {
+    const prepareDiff = () => {
+      setDiffContent(
+        DiffLibrary.createTwoFilesPatch(
+          '  ',
+          '  ',
+          props.template.data,
+          props.template.compareData,
+          props.diffVersion,
+          props.currentVersion,
+          { context: props.expanded ? Number.MAX_SAFE_INTEGER : 2 }
+        )
+      );
+    };
+
+    prepareDiff();
+  }, [props.template, props.expanded]); /* eslint-disable-line react-hooks/exhaustive-deps */
+
+  return (
+    <>
+      {!isNull(diffContent) && (
+        <Changes
+          diffText={`diff --git \n ${diffContent}`}
+          fileName={props.template.name}
+          status={props.template.status}
+          removeLoading={removeLoading}
+        />
+      )}
+    </>
+  );
+};
+
+export default DiffTemplate;
