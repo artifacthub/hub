cc495cae293205463544bdf7c215ef6dd0c93e43
Cintia Sanchez Garcia <cynthiasg@icloud.com>
2022-06-09 09:58:57 +0200

    Highlight selected template in templates explorer
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

---
+++ web/src/utils/isVisibleItemInContainer.ts
@@ -0,0 +1,13 @@
+import { isNull } from 'lodash';
+
+const isVisibleItemInContainer = function (ele: HTMLDivElement | null, container: HTMLDivElement | null): boolean {
+  if (!isNull(ele) && !isNull(container)) {
+    const { bottom, height, top } = ele.getBoundingClientRect();
+    const containerRect = container.getBoundingClientRect();
+
+    return top <= containerRect.top ? containerRect.top - top <= height : bottom - containerRect.bottom <= height;
+  }
+  return false;
+};
+
+export default isVisibleItemInContainer;
