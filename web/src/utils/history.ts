70de8b0463136624c70c96c5120c6f8865329792
Cintia Sánchez García <cynthiasg@icloud.com>
2022-08-24 11:28:41 +0200

    Load Google Analytics dynamically (#2244)
    Related to #2184
    
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/src/utils/history.ts
+++ web/src/utils/history.ts
@@ -1,18 +1,32 @@
 import { createBrowserHistory } from 'history';
+import { isNull } from 'lodash';
 
-import analytics from '../analytics/analytics';
 import { PKG_DETAIL_PATH } from './data';
+import getMetaTag from './getMetaTag';
 import updateMetaIndex from './updateMetaIndex';
 import notificationsDispatcher from './userNotificationsDispatcher';
 
 const history = createBrowserHistory();
+const analyticsConfig: string | null = getMetaTag('gaTrackingID');
+let analytics: any = null;
+
+// Imports analytics only when tracking id is defined
+if (!isNull(analyticsConfig)) {
+  import('../analytics/analytics').then((a) => {
+    analytics = a.default;
+  });
+}
 
 history.listen((location) => {
   // Updates meta tags every time that history is called for all locations except for package detail page
   if (!PKG_DETAIL_PATH.test(location.pathname)) {
     updateMetaIndex();
   }
-  analytics.page();
+
+  // Calls page view only when script has been loaded
+  if (!isNull(analytics)) {
+    analytics.page();
+  }
   // Clean notifications to change location
   notificationsDispatcher.dismissNotification(location.pathname);
 });
