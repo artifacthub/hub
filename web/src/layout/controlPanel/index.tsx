artifacthub/hub

commit aca2409b8ffdcf175cd6d9384f8924a5b8561dea

Upgrade react-router-dom to v6 (#2892)
Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

web/src/layout/controlPanel/index.tsx
@@ -2,7 +2,7 @@ import classnames from 'classnames';
 import isNull from 'lodash/isNull';
 import isUndefined from 'lodash/isUndefined';
 import { useContext, useEffect, useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useNavigate, useParams, useSearchParams } from 'react-router-dom';
 
 import { AppCtx, signOut, unselectOrg, updateOrg } from '../../context/AppCtx';
 import { Section } from '../../types';
@@ -17,28 +17,24 @@ import RepositoriesSection from './repositories';
 import SettingsSection from './settings';
 import UserContext from './UserContext';
 
-interface Props {
-  section?: string;
-  subsection?: string;
-  userAlias?: string;
-  organizationName?: string;
-  repoName?: string;
-  visibleModal?: string;
-  activePage?: string;
-}
-
 const DEFAULT_SECTION = 'repositories';
 
-const ControlPanelView = (props: Props) => {
-  const history = useHistory();
+const ControlPanelView = () => {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
   const { ctx, dispatch } = useContext(AppCtx);
-  const [activeSection, setActiveSection] = useState<string>(props.section || DEFAULT_SECTION);
-  const [activeSubsection, setActiveSubsection] = useState<string | null>(props.subsection || null);
+  const { section, subsection } = useParams();
+  const [activeSection, setActiveSection] = useState<string>(section || DEFAULT_SECTION);
+  const [activeSubsection, setActiveSubsection] = useState<string | null>(subsection || null);
   const [context, setContext] = useState<'user' | 'org' | null>(
     isUndefined(ctx.user) || isNull(ctx.user) ? null : isUndefined(ctx.prefs.controlPanel.selectedOrg) ? 'user' : 'org'
   );
   const isLoggedIn = !isUndefined(ctx.user) && !isNull(ctx.user);
   const [lastSelectedOrg, setLastSelectedOrg] = useState<string | undefined>(ctx.prefs.controlPanel.selectedOrg);
+  const userAlias = searchParams.get('user-alias');
+  const organizationName = searchParams.get('org-name');
+  const repoName = searchParams.get('repo-name');
+  const activePageParam = searchParams.get('page');
 
   const onAuthError = (): void => {
     alertDispatcher.postAlert({
@@ -46,7 +42,7 @@ const ControlPanelView = (props: Props) => {
       message: 'Sorry, you are not authorized to complete this action, please make sure you are signed in',
     });
     dispatch(signOut());
-    history.push(
+    navigate(
       `/?modal=login&redirect=/control-panel/${activeSection}${!isNull(activeSubsection) ? `/${activeSubsection}` : ''}`
     );
   };
@@ -54,8 +50,8 @@ const ControlPanelView = (props: Props) => {
   const checkIfAuthorizationIsActive = (newCtx: string): boolean => {
     if (
       newCtx === 'user' &&
-      props.subsection === 'authorization' &&
-      !isControlPanelSectionAvailable(newCtx, props.section, props.subsection) &&
+      subsection === 'authorization' &&
+      !isControlPanelSectionAvailable(newCtx, section, subsection) &&
       !isUndefined(lastSelectedOrg)
     ) {
       return true;
@@ -72,26 +68,29 @@ const ControlPanelView = (props: Props) => {
     if (ctx.user) {
       let context: 'user' | 'org' = isUndefined(ctx.prefs.controlPanel.selectedOrg) ? 'user' : 'org';
       if (
-        ctx.user.alias === props.userAlias &&
-        !isUndefined(props.userAlias) &&
-        props.userAlias !== '' &&
+        ctx.user.alias === userAlias &&
+        !isNull(userAlias) &&
+        userAlias !== '' &&
         !isUndefined(ctx.prefs.controlPanel.selectedOrg)
       ) {
         dispatch(unselectOrg());
         context = 'user';
       } else if (
-        !isUndefined(props.organizationName) &&
-        props.organizationName !== '' &&
-        ctx.prefs.controlPanel.selectedOrg !== props.organizationName
+        !isNull(organizationName) &&
+        organizationName !== '' &&
+        ctx.prefs.controlPanel.selectedOrg !== organizationName
       ) {
-        dispatch(updateOrg(props.organizationName));
+        dispatch(updateOrg(organizationName));
         context = 'org';
       }
 
-      if (isUndefined(props.repoName) && (!isUndefined(props.userAlias) || !isUndefined(props.organizationName))) {
-        history.replace({
-          search: '',
-        });
+      if (isNull(repoName) && (!isNull(userAlias) || !isNull(organizationName))) {
+        navigate(
+          {
+            search: '',
+          },
+          { replace: true }
+        );
       }
 
       if (ctx.prefs.controlPanel.selectedOrg) {
@@ -109,27 +108,27 @@ const ControlPanelView = (props: Props) => {
 
   useEffect(() => {
     if (ctx.user && !isNull(context)) {
-      if (isControlPanelSectionAvailable(context, props.section, props.subsection)) {
-        if (!isUndefined(props.section)) {
-          setActiveSection(props.section);
+      if (isControlPanelSectionAvailable(context, section, subsection)) {
+        if (!isUndefined(section)) {
+          setActiveSection(section);
         }
-        if (!isUndefined(props.subsection)) {
-          setActiveSubsection(props.subsection);
+        if (!isUndefined(subsection)) {
+          setActiveSubsection(subsection);
         }
       } else {
-        if (props.subsection !== 'authorization') {
-          history.replace(`/control-panel/${DEFAULT_SECTION}`);
+        if (subsection !== 'authorization') {
+          navigate(`/control-panel/${DEFAULT_SECTION}`, { replace: true });
         } else {
           if (isUndefined(lastSelectedOrg)) {
-            history.replace(`/control-panel/${DEFAULT_SECTION}`);
+            navigate(`/control-panel/${DEFAULT_SECTION}`, { replace: true });
           }
         }
       }
     }
-  }, [props.section, props.subsection, context]); /* eslint-disable-line react-hooks/exhaustive-deps */
+  }, [section, subsection, context]); /* eslint-disable-line react-hooks/exhaustive-deps */
 
   if (!isUndefined(ctx.user) && isNull(ctx.user)) {
-    history.push('/');
+    navigate('/');
   }
 
   if (isNull(context)) return null;
@@ -176,17 +175,24 @@ const ControlPanelView = (props: Props) => {
             {(() => {
               switch (activeSection) {
                 case 'repositories':
-                  return <RepositoriesSection {...props} onAuthError={onAuthError} />;
+                  return (
+                    <RepositoriesSection
+                      repoName={repoName}
+                      activePage={activePageParam}
+                      visibleModal={searchParams.get('modal')}
+                      onAuthError={onAuthError}
+                    />
+                  );
                 case 'organizations':
-                  return <OrganizationsSection {...props} onAuthError={onAuthError} />;
+                  return <OrganizationsSection activePage={activePageParam} onAuthError={onAuthError} />;
                 case 'members':
-                  return <MembersSection {...props} onAuthError={onAuthError} />;
+                  return <MembersSection activePage={activePageParam} onAuthError={onAuthError} />;
                 case 'settings':
                   return (
                     <SettingsSection
-                      {...props}
                       context={context}
                       activeSection={activeSection}
+                      subsection={subsection}
                       onAuthError={onAuthError}
                     />
                   );

web/src/layout/controlPanel/settings/userSettings/apiKeys/index.tsx
@@ -2,7 +2,7 @@ import isNull from 'lodash/isNull';
 import isUndefined from 'lodash/isUndefined';
 import { useEffect, useState } from 'react';
 import { MdAdd, MdAddCircle } from 'react-icons/md';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import API from '../../../../../api';
 import { APIKey, ErrorKind } from '../../../../../types';
@@ -26,7 +26,7 @@ interface ModalStatus {
 const DEFAULT_LIMIT = 10;
 
 const APIKeysSection = (props: Props) => {
-  const history = useHistory();
+  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
   const [apiKeysList, setApiKeysList] = useState<APIKey[] | undefined>(undefined);
   const [apiError, setApiError] = useState<string | JSX.Element | null>(null);
@@ -48,9 +48,7 @@ const APIKeysSection = (props: Props) => {
   };
 
   const updatePageNumber = () => {
-    history.replace({
-      search: `?page=${activePage}`,
-    });
+    navigate(`?page=${activePage}`, { replace: true });
   };
 
   async function getAPIKeys() {

web/src/layout/controlPanel/settings/webhooks/index.tsx
@@ -2,7 +2,7 @@ import isNull from 'lodash/isNull';
 import isUndefined from 'lodash/isUndefined';
 import { useContext, useEffect, useState } from 'react';
 import { MdAdd, MdAddCircle } from 'react-icons/md';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import API from '../../../../api';
 import { AppCtx } from '../../../../context/AppCtx';
@@ -27,7 +27,7 @@ interface VisibleForm {
 }
 
 const WebhooksSection = (props: Props) => {
-  const history = useHistory();
+  const navigate = useNavigate();
   const { ctx } = useContext(AppCtx);
   const [isGettingWebhooks, setIsGettingWebhooks] = useState(false);
   const [webhooks, setWebhooks] = useState<Webhook[] | undefined>(undefined);
@@ -48,10 +48,9 @@ const WebhooksSection = (props: Props) => {
   };
 
   const updatePageNumber = () => {
-    history.replace({
-      search: `?page=${activePage}`,
-    });
+    navigate(`?page=${activePage}`, { replace: true });
   };
+
   async function fetchWebhooks() {
     try {
       setIsGettingWebhooks(true);

web/src/layout/index.tsx
@@ -1,0 +1,95 @@
+import { isNull } from 'lodash';
+import { useState } from 'react';
+import { matchRoutes, Outlet, useLocation, useNavigate } from 'react-router-dom';
+
+import { AppCtxProvider } from '../context/AppCtx';
+import useOnLocationChange from '../hooks/useOnLocationChange';
+import useScrollRestorationFix from '../hooks/useScrollRestorationFix';
+import { HOME_ROUTES, PKG_DETAIL_PATH } from '../utils/data';
+import getMetaTag from '../utils/getMetaTag';
+import { history } from '../utils/history';
+import updateMetaIndex from '../utils/updateMetaIndex';
+import notificationsDispatcher from '../utils/userNotificationsDispatcher';
+import AlertController from './common/AlertController';
+import UserNotificationsController from './common/userNotifications';
+import BannerMOTD from './navigation/BannerMOTD';
+import Footer from './navigation/Footer';
+import Navbar from './navigation/Navbar';
+
+const Layout = () => {
+  const location = useLocation();
+
+  // init custom history object to allow navigation from
+  // anywhere in the react app (inside or outside components)
+  history.navigate = useNavigate();
+  history.location = location;
+
+  const analyticsConfig: string | null = getMetaTag('gaTrackingID');
+  let analytics: any = null;
+  const [isSearching, setIsSearching] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [scrollPosition, setScrollPosition] = useState<undefined | number>(undefined);
+  const [viewedPackage, setViewedPackage] = useState<undefined | string>(undefined);
+
+  const privateRoutes = [{ path: '/packages/starred' }, { path: '/control-panel/:section?/:subsection?' }];
+  const matchesWithPrivateRoutes = matchRoutes(privateRoutes, location);
+  const homeRoutes = ['/', ...HOME_ROUTES];
+
+  if (!isNull(analyticsConfig)) {
+    import('../analytics/analytics').then((a) => {
+      analytics = a.default;
+    });
+  }
+
+  useOnLocationChange((loc: Location) => {
+    // Updates meta tags every time that history is called for all locations except for package detail page
+    if (!PKG_DETAIL_PATH.test(loc.pathname)) {
+      updateMetaIndex();
+    }
+
+    // Calls page view only when script has been loaded
+    if (!isNull(analytics)) {
+      analytics.page();
+    }
+
+    // Clean notifications to change location
+    notificationsDispatcher.dismissNotification(location.pathname);
+  });
+
+  useScrollRestorationFix();
+
+  return (
+    <AppCtxProvider>
+      <div className="d-flex flex-column min-vh-100 position-relative whiteBranded">
+        <div className="visually-hidden visually-hidden-focusable">
+          <a href="#content">Skip to Main Content</a>
+        </div>
+        <AlertController />
+        <BannerMOTD />
+        <UserNotificationsController />
+        <Navbar
+          isSearching={isSearching}
+          privateRoute={!isNull(matchesWithPrivateRoutes)}
+          inHome={homeRoutes.includes(location.pathname)}
+        />
+        <div className="d-flex flex-column flex-grow-1">
+          <Outlet
+            context={{
+              isSearching,
+              setIsSearching,
+              isLoading,
+              setIsLoading,
+              scrollPosition,
+              setScrollPosition,
+              viewedPackage,
+              setViewedPackage,
+            }}
+          />
+        </div>
+        <Footer isHidden={isSearching || isLoading} />
+      </div>
+    </AppCtxProvider>
+  );
+};
+
+export default Layout;

web/src/layout/navigation/Navbar.tsx
@@ -2,7 +2,7 @@ import classnames from 'classnames';
 import isNull from 'lodash/isNull';
 import isUndefined from 'lodash/isUndefined';
 import { useContext, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 import { AppCtx } from '../../context/AppCtx';
 import getMetaTag from '../../utils/getMetaTag';
@@ -18,26 +18,24 @@ import UserAuthDropdown from './UserAuthDropdown';
 
 interface Props {
   isSearching: boolean;
-  fromHome?: boolean;
-  searchText?: string;
-  redirect?: string;
+  inHome?: boolean;
   privateRoute?: boolean;
-  visibleModal?: string;
 }
 
 const Navbar = (props: Props) => {
   const { ctx } = useContext(AppCtx);
-  const openLogInModal =
-    (!isUndefined(props.redirect) && isNull(ctx.user)) ||
-    (!isUndefined(props.visibleModal) && props.visibleModal === 'login');
+  const [searchParams] = useSearchParams();
+  const redirect = searchParams.get('redirect');
+  const visibleModal = searchParams.get('modal');
+  const openLogInModal = (!isNull(redirect) && isNull(ctx.user)) || (!isNull(visibleModal) && visibleModal === 'login');
   const [openSignUp, setOpenSignUp] = useState<boolean>(false);
   const [openLogIn, setOpenLogIn] = useState<boolean>(openLogInModal);
   const [openTips, setOpenTips] = useState<boolean>(false);
   useEffect(() => {
-    if (!isUndefined(props.redirect) && isNull(ctx.user)) {
+    if (!isNull(redirect) && isNull(ctx.user)) {
       setOpenLogIn(true);
     }
-  }, [props.redirect]); /* eslint-disable-line react-hooks/exhaustive-deps */
+  }, [redirect]); /* eslint-disable-line react-hooks/exhaustive-deps */
 
   const logo = getMetaTag('websiteLogo');
   const siteName = getMetaTag('siteName');
@@ -47,7 +45,7 @@ const Navbar = (props: Props) => {
     <>
       <nav
         className={classnames('navbar navbar-top navbar-expand-lg navbar-dark border-top-0 p-2 p-sm-3', styles.navbar, {
-          [`bg-transparent w-100 position-absolute ${styles.homeNavbar}`]: props.fromHome,
+          [`bg-transparent w-100 position-absolute ${styles.homeNavbar}`]: props.inHome,
         })}
       >
         <div className="container-lg px-0 px-sm-4 px-lg-0">
@@ -65,12 +63,11 @@ const Navbar = (props: Props) => {
             />
           </div>
 
-          {isUndefined(props.fromHome) && (
+          {isUndefined(props.inHome) && (
             <SearchBar
               size="normal"
               formClassName={`mx-2 me-md-auto my-3 my-md-0 flex-grow-1 pe-0 pe-sm-4 ${styles.search}`}
               isSearching={props.isSearching}
-              tsQueryWeb={props.searchText}
               openTips={openTips}
               setOpenTips={setOpenTips}
             />
@@ -163,15 +160,10 @@ const Navbar = (props: Props) => {
       {openSignUp && <SignUp openSignUp={openSignUp} setOpenSignUp={setOpenSignUp} />}
 
       {openLogIn && (
-        <LogIn
-          openLogIn={openLogIn}
-          setOpenLogIn={setOpenLogIn}
-          redirect={props.redirect}
-          visibleModal={props.visibleModal}
-        />
+        <LogIn openLogIn={openLogIn} setOpenLogIn={setOpenLogIn} redirect={redirect} visibleModal={visibleModal} />
       )}
 
-      {isUndefined(props.fromHome) && <SearchTipsModal size="normal" openTips={openTips} setOpenTips={setOpenTips} />}
+      {isUndefined(props.inHome) && <SearchTipsModal size="normal" openTips={openTips} setOpenTips={setOpenTips} />}
     </>
   );
 };

commit e671898a783f224857dbfe4917c091079798cdb5

Fix regression in UI introduced in #2892
Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

web/public/index.html
@@ -23,6 +23,8 @@
       <meta name="artifacthub:allowUserSignUp" content="true" />
       <meta name="artifacthub:reportURL" content="https://github.com/artifacthub/hub/issues/new?labels=abuse+report&template=report-abuse.md" />
       <meta name="artifacthub:bannersURL" content="https://cncf.github.io/banners/banners.yml" />
+      <meta name="artifacthub:motd" content="This is a staging environment used for testing. The production environment can be found at https://artifacthub.io">
+      <meta name="artifacthub:motdSeverity" content="warning">
     <% } else { %>
       <title>{{ .title }}</title>
       <meta name="description" content="{{ .description }}" />

web/src/layout/controlPanel/index.tsx
@@ -190,6 +190,7 @@ const ControlPanelView = () => {
                 case 'settings':
                   return (
                     <SettingsSection
+                      activePage={activePageParam}
                       context={context}
                       activeSection={activeSection}
                       subsection={subsection}

web/src/layout/controlPanel/settings/index.tsx
@@ -2,6 +2,7 @@ import OrganizationSettingsSection from './orgSettings';
 import UserSettingsSection from './userSettings';
 
 interface Props {
+  activePage: string | null;
   context: 'user' | 'org';
   subsection?: string;
   activeSection?: string;

web/src/layout/controlPanel/settings/orgSettings/index.tsx
@@ -8,6 +8,7 @@ import AuthorizationSection from './authorization';
 import ProfileSection from './profile';
 
 interface Props {
+  activePage: string | null;
   activeSection?: string;
   subsection?: string;
   onAuthError: () => void;

web/src/layout/controlPanel/settings/userSettings/apiKeys/index.tsx
@@ -14,7 +14,7 @@ import APIKeyCard from './Card';
 import APIKeyModal from './Modal';
 
 interface Props {
-  activePage?: string;
+  activePage: string | null;
   onAuthError: () => void;
 }
 

web/src/layout/controlPanel/settings/userSettings/index.tsx
@@ -9,6 +9,7 @@ import ProfileSection from './profile';
 import SubscriptionsSection from './subscriptions';
 
 interface Props {
+  activePage: string | null;
   activeSection?: string;
   subsection?: string;
   onAuthError: () => void;

web/src/layout/controlPanel/settings/webhooks/index.tsx
@@ -15,7 +15,7 @@ import WebhookForm from './Form';
 import styles from './WebhooksSection.module.css';
 
 interface Props {
-  activePage?: string;
+  activePage: string | null;
   onAuthError: () => void;
 }
 

web/src/layout/index.tsx
@@ -67,26 +67,28 @@ const Layout = () => {
         <AlertController />
         <BannerMOTD />
         <UserNotificationsController />
-        <Navbar
-          isSearching={isSearching}
-          privateRoute={!isNull(matchesWithPrivateRoutes)}
-          inHome={homeRoutes.includes(location.pathname)}
-        />
         <div className="d-flex flex-column flex-grow-1">
-          <Outlet
-            context={{
-              isSearching,
-              setIsSearching,
-              isLoading,
-              setIsLoading,
-              scrollPosition,
-              setScrollPosition,
-              viewedPackage,
-              setViewedPackage,
-            }}
+          <Navbar
+            isSearching={isSearching}
+            privateRoute={!isNull(matchesWithPrivateRoutes)}
+            inHome={homeRoutes.includes(location.pathname)}
           />
+          <div className="d-flex flex-column flex-grow-1">
+            <Outlet
+              context={{
+                isSearching,
+                setIsSearching,
+                isLoading,
+                setIsLoading,
+                scrollPosition,
+                setScrollPosition,
+                viewedPackage,
+                setViewedPackage,
+              }}
+            />
+          </div>
+          <Footer isHidden={isSearching || isLoading} />
         </div>
-        <Footer isHidden={isSearching || isLoading} />
       </div>
     </AppCtxProvider>
   );

web/src/layout/navigation/Navbar.tsx
@@ -18,7 +18,7 @@ import UserAuthDropdown from './UserAuthDropdown';
 
 interface Props {
   isSearching: boolean;
-  inHome?: boolean;
+  inHome: boolean;
   privateRoute?: boolean;
 }
 
@@ -63,7 +63,7 @@ const Navbar = (props: Props) => {
             />
           </div>
 
-          {isUndefined(props.inHome) && (
+          {!props.inHome && (
             <SearchBar
               size="normal"
               formClassName={`mx-2 me-md-auto my-3 my-md-0 flex-grow-1 pe-0 pe-sm-4 ${styles.search}`}
@@ -163,7 +163,7 @@ const Navbar = (props: Props) => {
         <LogIn openLogIn={openLogIn} setOpenLogIn={setOpenLogIn} redirect={redirect} visibleModal={visibleModal} />
       )}
 
-      {isUndefined(props.inHome) && <SearchTipsModal size="normal" openTips={openTips} setOpenTips={setOpenTips} />}
+      {!props.inHome && <SearchTipsModal size="normal" openTips={openTips} setOpenTips={setOpenTips} />}
     </>
   );
 };

web/src/layout/navigation/__snapshots__/Navbar.test.tsx.snap
@@ -3,7 +3,7 @@
 exports[`Navbar creates snapshot 1`] = `
 <DocumentFragment>
   <nav
-    class="navbar navbar-top navbar-expand-lg navbar-dark border-top-0 p-2 p-sm-3 navbar"
+    class="navbar navbar-top navbar-expand-lg navbar-dark border-top-0 p-2 p-sm-3 navbar bg-transparent w-100 position-absolute homeNavbar"
   >
     <div
       class="container-lg px-0 px-sm-4 px-lg-0"
@@ -26,93 +26,6 @@ exports[`Navbar creates snapshot 1`] = `
           </div>
         </a>
         <div />
-      </div>
-      <div
-        class="position-relative mx-2 me-md-auto my-3 my-md-0 flex-grow-1 pe-0 pe-sm-4 search"
-      >
-        <div
-          aria-controls="search-list"
-          aria-expanded="false"
-          aria-haspopup="listbox"
-          aria-owns="search-list"
-          class="d-flex align-items-center overflow-hidden searchBar lh-base bg-white searchBar normal"
-          role="combobox"
-        >
-          <div
-            class="d-flex align-items-center iconWrapper"
-            data-testid="searchBarIcon"
-          >
-            <svg
-              fill="none"
-              height="1em"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              viewBox="0 0 24 24"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle
-                cx="11"
-                cy="11"
-                r="8"
-              />
-              <line
-                x1="21"
-                x2="16.65"
-                y1="21"
-                y2="16.65"
-              />
-            </svg>
-          </div>
-          <input
-            aria-label="Search packages"
-            autocapitalize="none"
-            autocomplete="off"
-            autocorrect="off"
-            class="flex-grow-1 ps-2 ps-md-0 border-0 shadow-none bg-transparent input"
-            placeholder="Search packages"
-            spellcheck="false"
-            type="text"
-            value=""
-          />
-          <div
-            class="d-none"
-            tabindex="0"
-          >
-            <div
-              aria-live="polite"
-            />
-          </div>
-          <button
-            aria-label="Close"
-            class="btn-close lh-lg ps-2 pe-3 inputClean invisible"
-            type="button"
-          />
-          <div
-            class="d-none d-sm-block position-absolute text-dark tipIcon"
-          >
-            <button
-              aria-label="Open search tips modal"
-              class="btn btn-link p-2 text-light"
-            >
-              <svg
-                fill="currentColor"
-                height="1em"
-                stroke="currentColor"
-                stroke-width="0"
-                viewBox="0 0 512 512"
-                width="1em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 448c-110.532 0-200-89.431-200-200 0-110.495 89.472-200 200-200 110.491 0 200 89.471 200 200 0 110.53-89.431 200-200 200zm107.244-255.2c0 67.052-72.421 68.084-72.421 92.863V300c0 6.627-5.373 12-12 12h-45.647c-6.627 0-12-5.373-12-12v-8.659c0-35.745 27.1-50.034 47.579-61.516 17.561-9.845 28.324-16.541 28.324-29.579 0-17.246-21.999-28.693-39.784-28.693-23.189 0-33.894 10.977-48.942 29.969-4.057 5.12-11.46 6.071-16.666 2.124l-27.824-21.098c-5.107-3.872-6.251-11.066-2.644-16.363C184.846 131.491 214.94 112 261.794 112c49.071 0 101.45 38.304 101.45 88.8zM298 368c0 23.159-18.841 42-42 42s-42-18.841-42-42 18.841-42 42-42 42 18.841 42 42z"
-                />
-              </svg>
-            </button>
-          </div>
-        </div>
       </div>
       <div
         class="collapse navbar-collapse"

web/src/layout/controlPanel/settings/index.test.tsx
@@ -6,6 +6,7 @@ jest.mock('./userSettings', () => () => <div>user</div>);
 jest.mock('./orgSettings', () => () => <div>org</div>);
 
 const defaultProps = {
+  activePage: '1',
   onAuthError: jest.fn(),
 };
 

web/src/layout/controlPanel/settings/orgSettings/index.test.tsx
@@ -5,6 +5,7 @@ import OrganizationSettingsSection from './index';
 jest.mock('../../../common/SectionPanel', () => (props: any) => <div>{props.defaultSection}</div>);
 
 const defaultProps = {
+  activePage: null,
   activeSection: 'user',
   onAuthError: jest.fn(),
 };

web/src/layout/controlPanel/settings/userSettings/apiKeys/index.test.tsx
@@ -16,6 +16,7 @@ const getMockAPIKeys = (fixtureId: string) => {
 const onAuthErrorMock = jest.fn();
 
 const defaultProps = {
+  activePage: null,
   onAuthError: onAuthErrorMock,
 };
 

web/src/layout/controlPanel/settings/userSettings/index.test.tsx
@@ -5,6 +5,7 @@ import UserSettingsSection from './index';
 jest.mock('../../../common/SectionPanel', () => (props: any) => <div>{props.defaultSection}</div>);
 
 const defaultProps = {
+  activePage: null,
   activeSection: 'user',
   onAuthError: jest.fn(),
 };

web/src/layout/controlPanel/settings/webhooks/index.test.tsx
@@ -16,6 +16,7 @@ const getMockWebhooks = (fixtureId: string) => {
 const mockOnAuthError = jest.fn();
 
 const defaultProps = {
+  activePage: null,
   onAuthError: mockOnAuthError,
 };
 

web/src/layout/navigation/Navbar.test.tsx
@@ -13,7 +13,7 @@ jest.mock('react-router-dom', () => ({
 
 const defaultProps = {
   isSearching: false,
-  fromHome: true,
+  inHome: true,
 };
 
 const mockCtxLoggedIn = {