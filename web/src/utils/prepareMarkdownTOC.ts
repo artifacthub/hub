6cdc250e59c48f092e58257e78efaa3270090dd7
Cintia Sánchez García <cynthiasg@icloud.com>
2023-05-23 10:36:06 +0200

    Upgrade frontend dependencies (#3069)
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/src/utils/prepareMarkdownTOC.ts
+++ web/src/utils/prepareMarkdownTOC.ts
@@ -1,13 +1,13 @@
-import { TOCEntryItem } from '../types';
+import remarkParse from 'remark-parse';
+import { unified } from 'unified';
+
 import { extractReadmeTOC } from './extractReadmeTOC';
 
-const unified = require('unified');
 const strip = require('remark-strip-html');
-const markdown = require('remark-parse');
 const unlink = require('remark-unlink');
-const processor = unified().use(markdown).use(strip).use(unlink).use(extractReadmeTOC);
+const processor = unified().use(remarkParse).use(strip).use(unlink).use(extractReadmeTOC);
 
-const prepareMarkdownTOC = (md: string): TOCEntryItem[] => {
+const prepareMarkdownTOC = (md: string): any => {
   const node = processor.parse(md);
   const tree = processor.runSync(node);
   return tree;
