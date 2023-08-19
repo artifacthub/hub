fe4fda1ba819c00872c9b1520f5844fa7e0f2c0e
Cintia Sanchez Garcia <cynthiasg@icloud.com>
2023-08-19 17:45:43 +0200

    Upgrade frontend dependencies
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/src/setupTests.ts
+++ web/src/setupTests.ts
@@ -1,7 +1,7 @@
 // react-testing-library renders your components to document.body,
 // this adds jest-dom's custom assertions
 
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 
 const noop = () => {};
 Object.defineProperty(window, 'scrollTo', { value: noop, writable: true });
