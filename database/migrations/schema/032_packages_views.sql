f65d8ac04d16d51b6e1a53958620063514ce9c66
Sergio Castaño Arteaga <tegioz@icloud.com>
2024-07-15 14:20:01 +0200

    Update db migration script to support pg_partman 5 (#3942)
    Closes #3931
    
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- database/migrations/schema/032_packages_views.sql
+++ database/migrations/schema/032_packages_views.sql
@@ -1,5 +1,3 @@
-create extension pg_partman;
-
 create table if not exists package_views (
     package_id uuid not null references package on delete cascade,
     version text not null,
@@ -8,10 +6,6 @@ create table if not exists package_views (
     unique (package_id, version, day)
 ) partition by range (day);
 
-select create_parent('public.package_views', 'day', 'native', 'monthly', p_start_partition := current_date::text);
-
 ---- create above / drop below ----
 
 drop table if exists package_views;
-drop table template_public_package_views;
-delete from part_config where parent_table = 'public.package_views';
---
+++ database/migrations/schema/034_partman.sql
@@ -0,0 +1,34 @@
+create schema partman;
+create extension pg_partman schema partman;
+
+do language plpgsql $$
+declare
+    v_pg_partman_version text;
+begin
+    select extversion from pg_extension where extname = 'pg_partman' into v_pg_partman_version;
+    case
+        when starts_with(v_pg_partman_version, '4.') then
+            perform partman.create_parent(
+                'public.package_views',
+                'day',
+                'native',
+                'monthly',
+                p_start_partition := current_date::text
+            );
+        when starts_with(v_pg_partman_version, '5.') then
+            perform partman.create_parent(
+                'public.package_views',
+                'day',
+                '1 month',
+                p_start_partition := current_date::text
+            );
+        else
+            raise exception 'pg_partman version not supported';
+    end case;
+end
+$$;
+
+---- create above / drop below ----
+
+drop extension pg_partman;
+drop schema partman cascade;
