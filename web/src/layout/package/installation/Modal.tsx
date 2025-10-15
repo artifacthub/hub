71753d9d7c6cabc914dd2d4fbe5b38c53921eecf
Cintia Sánchez García <cynthiasg@icloud.com>
2025-10-15 17:29:27 +0200

    Display install button by default
    Signed-off-by: Cintia Sánchez García <cynthiasg@icloud.com>

--- web/src/layout/package/installation/Modal.tsx
+++ web/src/layout/package/installation/Modal.tsx
@@ -38,8 +38,11 @@ const InstallationModal = (props: Props) => {
   const navigate = useNavigate();
   const location = useLocation();
   const [openStatus, setOpenStatus] = useState<boolean>(false);
-  const [installMethods, setInstallMethods] = useState<InstallMethodOutput | null>(null); // undefined ???
-  const isDisabled = !isNull(installMethods) && !isUndefined(installMethods.errorMessage);
+  const [installMethods, setInstallMethods] = useState<InstallMethodOutput | null>(null);
+  const isLoading = isNull(installMethods);
+  const errorMessage = installMethods?.errorMessage;
+  const hasError = !isUndefined(errorMessage);
+  const isDisabled = isLoading || hasError;
 
   const onOpenModal = () => {
     if (!isNull(installMethods) && installMethods.methods.length > 0) {
@@ -82,8 +85,7 @@ const InstallationModal = (props: Props) => {
     }
   }, [installMethods]);
 
-  if (isNull(installMethods) || (installMethods.methods.length === 0 && isUndefined(installMethods.errorMessage)))
-    return null;
+  if (installMethods && installMethods.methods.length === 0 && isUndefined(installMethods.errorMessage)) return null;
 
   return (
     <>
@@ -98,6 +100,7 @@ const InstallationModal = (props: Props) => {
             onClick={onOpenModal}
             aria-label="Open installation modal"
             aria-disabled={isDisabled}
+            disabled={isDisabled}
           >
             <div className="d-flex align-items-center justify-content-center">
               <FiDownload className="me-2" />
@@ -105,141 +108,143 @@ const InstallationModal = (props: Props) => {
             </div>
           </button>
         }
-        visibleTooltip={isDisabled}
-        tooltipMessage={installMethods.errorMessage || ''}
+        visibleTooltip={hasError}
+        tooltipMessage={errorMessage || ''}
         active
       />
 
-      <Modal
-        header={
-          <ModalHeader
-            displayName={props.package!.displayName}
-            name={props.package!.name}
-            logoImageId={props.package!.logoImageId}
-            repoKind={props.package!.repository.kind}
-          />
-        }
-        onClose={onCloseModal}
-        open={openStatus}
-      >
-        <>
-          {installMethods.methods.length > 0 && (
-            <>
-              {props.package && props.package.prerelease && (
-                <div className="alert alert-warning mt-1 mb-4" role="alert">
-                  This package version is a <span className="fw-bold">pre-release</span> and it is not ready for
-                  production use.
-                </div>
-              )}
+      {installMethods && (
+        <Modal
+          header={
+            <ModalHeader
+              displayName={props.package!.displayName}
+              name={props.package!.name}
+              logoImageId={props.package!.logoImageId}
+              repoKind={props.package!.repository.kind}
+            />
+          }
+          onClose={onCloseModal}
+          open={openStatus}
+        >
+          <>
+            {installMethods.methods.length > 0 && (
+              <>
+                {props.package && props.package.prerelease && (
+                  <div className="alert alert-warning mt-1 mb-4" role="alert">
+                    This package version is a <span className="fw-bold">pre-release</span> and it is not ready for
+                    production use.
+                  </div>
+                )}
 
-              <Tabs
-                tabs={installMethods.methods.map((method: InstallMethod) => ({
-                  name: method.label,
-                  title: method.title,
-                  shortTitle: method.shortTitle,
-                  content: (
-                    <>
-                      {(() => {
-                        switch (method.kind) {
-                          case InstallMethodKind.PublisherInstructions:
-                            return <PublisherInstructionsInstall install={method.props.install!} />;
-                          case InstallMethodKind.Helm:
-                            return (
-                              <HelmInstall
-                                name={method.props.name!}
-                                version={method.props.version}
-                                repository={method.props.repository!}
-                                contentUrl={method.props.contentUrl}
-                                label={method.label}
-                              />
-                            );
-                          case InstallMethodKind.HelmOCI:
-                            return (
-                              <HelmOCIInstall
-                                name={method.props.name!}
-                                version={method.props.version}
-                                repository={method.props.repository!}
-                              />
-                            );
-                          case InstallMethodKind.OLM:
-                            return (
-                              <OLMInstall
-                                name={method.props.name!}
-                                defaultChannel={method.props.defaultChannel}
-                                channels={method.props.channels}
-                                isGlobalOperator={method.props.isGlobalOperator}
-                                isPrivate={method.props.isPrivate}
-                              />
-                            );
-                          case InstallMethodKind.OLMOCI:
-                            return (
-                              <OLMOCIInstall
-                                name={method.props.name!}
-                                repository={method.props.repository!}
-                                defaultChannel={method.props.defaultChannel}
-                                channels={method.props.channels}
-                              />
-                            );
-                          case InstallMethodKind.Falco:
-                            return (
-                              <FalcoInstall
-                                normalizedName={method.props.normalizedName!}
-                                isPrivate={method.props.isPrivate}
-                              />
-                            );
-                          case InstallMethodKind.Krew:
-                            return <KrewInstall name={method.props.name!} repository={method.props.repository!} />;
-                          case InstallMethodKind.HelmPlugin:
-                            return <HelmPluginInstall repository={method.props.repository!} />;
-                          case InstallMethodKind.Tekton:
-                            return (
-                              <TektonInstall
-                                contentUrl={method.props.contentUrl!}
-                                isPrivate={method.props.isPrivate}
-                                repository={method.props.repository!}
-                              />
-                            );
-                          case InstallMethodKind.Kubewarden:
-                            return (
-                              <KubewardenInstall images={method.props.images!} isPrivate={method.props.isPrivate} />
-                            );
-                          case InstallMethodKind.KustomizeGatekeeperInstall:
-                            return (
-                              <KustomizeGatekeeperInstall
-                                repository={method.props.repository!}
-                                relativePath={method.props.relativePath!}
-                              />
-                            );
-                          case InstallMethodKind.KubectlGatekeeperInstall:
-                            return (
-                              <KubectlGatekeeperInstall
-                                repository={method.props.repository!}
-                                examples={method.props.examples}
-                                relativePath={method.props.relativePath!}
-                              />
-                            );
-                          case InstallMethodKind.KubeArmor:
-                            return (
-                              <KubeArmorInstall
-                                repository={method.props.repository!}
-                                policies={method.props.policies}
-                                relativePath={method.props.relativePath!}
-                              />
-                            );
-                          default:
-                            return null;
-                        }
-                      })()}
-                    </>
-                  ),
-                }))}
-                active={installMethods.methods[0].label}
-                noDataContent="Sorry, the information for installation is missing."
-              />
-            </>
-          )}
-        </>
-      </Modal>
+                <Tabs
+                  tabs={installMethods.methods.map((method: InstallMethod) => ({
+                    name: method.label,
+                    title: method.title,
+                    shortTitle: method.shortTitle,
+                    content: (
+                      <>
+                        {(() => {
+                          switch (method.kind) {
+                            case InstallMethodKind.PublisherInstructions:
+                              return <PublisherInstructionsInstall install={method.props.install!} />;
+                            case InstallMethodKind.Helm:
+                              return (
+                                <HelmInstall
+                                  name={method.props.name!}
+                                  version={method.props.version}
+                                  repository={method.props.repository!}
+                                  contentUrl={method.props.contentUrl}
+                                  label={method.label}
+                                />
+                              );
+                            case InstallMethodKind.HelmOCI:
+                              return (
+                                <HelmOCIInstall
+                                  name={method.props.name!}
+                                  version={method.props.version}
+                                  repository={method.props.repository!}
+                                />
+                              );
+                            case InstallMethodKind.OLM:
+                              return (
+                                <OLMInstall
+                                  name={method.props.name!}
+                                  defaultChannel={method.props.defaultChannel}
+                                  channels={method.props.channels}
+                                  isGlobalOperator={method.props.isGlobalOperator}
+                                  isPrivate={method.props.isPrivate}
+                                />
+                              );
+                            case InstallMethodKind.OLMOCI:
+                              return (
+                                <OLMOCIInstall
+                                  name={method.props.name!}
+                                  repository={method.props.repository!}
+                                  defaultChannel={method.props.defaultChannel}
+                                  channels={method.props.channels}
+                                />
+                              );
+                            case InstallMethodKind.Falco:
+                              return (
+                                <FalcoInstall
+                                  normalizedName={method.props.normalizedName!}
+                                  isPrivate={method.props.isPrivate}
+                                />
+                              );
+                            case InstallMethodKind.Krew:
+                              return <KrewInstall name={method.props.name!} repository={method.props.repository!} />;
+                            case InstallMethodKind.HelmPlugin:
+                              return <HelmPluginInstall repository={method.props.repository!} />;
+                            case InstallMethodKind.Tekton:
+                              return (
+                                <TektonInstall
+                                  contentUrl={method.props.contentUrl!}
+                                  isPrivate={method.props.isPrivate}
+                                  repository={method.props.repository!}
+                                />
+                              );
+                            case InstallMethodKind.Kubewarden:
+                              return (
+                                <KubewardenInstall images={method.props.images!} isPrivate={method.props.isPrivate} />
+                              );
+                            case InstallMethodKind.KustomizeGatekeeperInstall:
+                              return (
+                                <KustomizeGatekeeperInstall
+                                  repository={method.props.repository!}
+                                  relativePath={method.props.relativePath!}
+                                />
+                              );
+                            case InstallMethodKind.KubectlGatekeeperInstall:
+                              return (
+                                <KubectlGatekeeperInstall
+                                  repository={method.props.repository!}
+                                  examples={method.props.examples}
+                                  relativePath={method.props.relativePath!}
+                                />
+                              );
+                            case InstallMethodKind.KubeArmor:
+                              return (
+                                <KubeArmorInstall
+                                  repository={method.props.repository!}
+                                  policies={method.props.policies}
+                                  relativePath={method.props.relativePath!}
+                                />
+                              );
+                            default:
+                              return null;
+                          }
+                        })()}
+                      </>
+                    ),
+                  }))}
+                  active={installMethods.methods[0].label}
+                  noDataContent="Sorry, the information for installation is missing."
+                />
+              </>
+            )}
+          </>
+        </Modal>
+      )}
     </>
   );
 };
